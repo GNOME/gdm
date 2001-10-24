04eef84f74899f19fe10aa4d6a19554cfb85b6a3
George Lebl <jirka@5z.com>
2001-10-24 18:06:22 +0000

    add a small utility for safely making temp files
    Wed Oct 24 11:02:55 2001  George Lebl <jirka@5z.com>
    
    	* utils/gdmkstemp.c, utils/Makefile.am:  add a small utility
    	  for safely making temp files
    
    Tue Oct 23 15:17:23 2001  George Lebl <jirka@5z.com>

---
+++ utils/gdmmkstemp.c
@@ -0,0 +1,32 @@
+/*
+ * 	 gdmmkstemp.c by the Queen of England
+ *	 A utility to do mkstemp from a script.
+ *       
+ *	 Copyright (c) 2001 by Queen of England
+ *
+ *       This program is free software; you can redistribute it and/or
+ *       modify it under the terms of the GNU General Public License
+ *       as published by the Free Software Foundation; either version
+ *       2 of the License, or (at your option) any later version.
+ */
+
+#include "config.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int 
+main (int argc, char *argv[])
+{
+	char template[] = "/tmp/gdm-XXXXXX";
+	int i;
+
+	i = mkstemp (template);
+	if (i < 0)
+		return 1;
+	
+	fchown (i, 0600);
+
+	printf ("%s\n", template);
+	close (i)
+	return 0;
+}
