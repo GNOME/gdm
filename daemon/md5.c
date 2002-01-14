8a4ff67778195c0c54f42b0ef02970bf018e0a9f
George Lebl <jirka@5z.com>
2002-01-14 06:17:31 +0000

    Release 2.3.90.1
    Sun Jan 13 21:58:36 2002  George Lebl <jirka@5z.com>
    
    	* Release 2.3.90.1
    
    	* daemon/md5.c: a bit of cleanup

--- daemon/md5.c
+++ daemon/md5.c
@@ -19,6 +19,7 @@
  *
  */
 
+#include "config.h"
 #include <string.h>		/* for memcpy() */
 
 #ifdef __LINUX__
@@ -31,12 +32,11 @@
 #define byteReverse(buf, len)	/* Nothing */
 #else
 
-void byteReverse(unsigned char *buf, unsigned longs);
-
 /*
  * Note: this code is harmless on little-endian machines.
  */
-void byteReverse(unsigned char *buf, unsigned longs)
+static void
+byteReverse(unsigned char *buf, unsigned longs)
 {
     uint32 t;
     do {
