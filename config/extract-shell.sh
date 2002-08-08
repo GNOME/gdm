5898b345f7553942b60342cce9c698ab5ba573f1
George Lebl <jirka@5z.com>
2002-08-08 16:26:34 +0000

    DAMN PEOPLE FUCKING COMMITTING THINGS WITHOUT CHANGELOG!  SOMEONE BACK IN FEBRUARY CHANGED THE GENERATED FILE AND THERE WAS NO TRANSLATION UP TILL NOW BECAUSE OF IT.  IF ANYONE WILL COMMIT WITHOUT A CHANGELOG AGAIN I WILL RIP THEM A NEW ASSHOLE.  (Sorry for the shouting, I just have a lot to do today and didn't want   to spend time on this:)  -George
    Thu Aug 08 09:33:50 2002  George Lebl <jirka@5z.com>
    
    	* config/gettextfoo.h, config/XKeepsCrashing:  Fix the message bits.

--- config/extract-shell.sh
+++ config/extract-shell.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+echo "/* DON\'T CHANGE THIS BY HAND! CHANGE THE SCRIPT THIS IS GENERATED */"
+echo "/* ALWAYS ADD A CHANGELOG OR I WILL PERSONALLY KICK YOUR ASS! */"
 grep gettext | sed 's/^[^"]*\("[^"]*"\).*$/const char *foo = N_(\1);/'
--- config/gettextfoo.h
+++ config/gettextfoo.h
@@ -1,6 +1,8 @@
+/* DON\'T CHANGE THIS BY HAND! CHANGE THE SCRIPT THIS IS GENERATED */
+/* ALWAYS ADD A CHANGELOG OR I WILL PERSONALLY KICK YOUR ASS! */
 const char *foo = N_("I cannot start the X server (your graphical interface).  It is likely that it is not set up correctly.  You will need to log in on a console and rerun the X configuration program.  Then restart GDM.");
 const char *foo = N_("Would you like me to try to run the X configuration program?  Note that you will need the root password for this.");
-const char *foo = N_("Please type in the root (privilaged user) password.");
+const char *foo = N_("Please type in the root (privileged user) password.");
 const char *foo = N_("I will now try to restart the X server again.");
 const char *foo = N_("I will disable this X server for now.  Restart GDM when it is configured correctly.");
 const char *foo = N_("I cannot start the X server (your graphical interface).  It is likely that it is not set up correctly.  Would you like to view the X server output to diagnose the problem?");
