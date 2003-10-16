f67e41aff62e56e997c0acf4297a3a0a41fe22cc
George Lebl <jirka@5z.com>
2003-10-16 17:41:05 +0000

    Release 2.4.4.4
    Thu Oct 16 09:55:55 2003  George Lebl <jirka@5z.com>
    
    	* Release 2.4.4.4
    
    	* config/shell-extract.sh: fix extracting of text from
    	  shell scripts.

--- config/extract-shell.sh
+++ config/extract-shell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "/* DON\'T CHANGE THIS BY HAND! CHANGE THE SCRIPT THIS IS GENERATED */"
 echo "/* ALWAYS ADD A CHANGELOG OR I WILL PERSONALLY KICK YOUR ASS! */"
-grep gettextfunc | fgrep -v 'gettextfunc ()' | sed 's/^[^"]*\("[^"]*"\).*$/const char *foo = N_(\1);/'
+grep gettextfunc | fgrep -v 'gettextfunc ()' | sed 's/^.*gettextfunc[^"]*\("[^"]*"\).*$/const char *foo = N_(\1);/'
