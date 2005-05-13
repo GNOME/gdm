0d5f421a8f16092eb35302434a88f0241fba6974
Brian Cameron <brian.cameron@sun.com>
2005-05-13 18:06:53 +0000

    Fix gdm2 so it works better on BSD and MacOS. Upon getting EOF on a
    2005-05-13  Brian Cameron  <brian.cameron@sun.com>
    
            * daemon/gdm-net.c: Fix gdm2 so it works better
              on BSD and MacOS.  Upon getting EOF on a socket/pipe/etc.
              G_IO_IN is always set and G_IO_HUP may not be set.
              Therefore, the 0 return from read() is important.  If
              we get a length <= 0 from read() we force the socket
              to be closed.  This prevents gdm-binary from causing
              high CPU-load after logout.  Fixes bug #172491.
              Fix posted by Joe Marcus Clarke.

--- daemon/gdm-net.c
+++ daemon/gdm-net.c
@@ -76,14 +76,14 @@ struct _GdmConnection {
 };
 
 static gboolean
-close_if_needed (GdmConnection *conn, GIOCondition cond)
+close_if_needed (GdmConnection *conn, GIOCondition cond, gboolean error)
 {
 	/* non-subconnections are never closed */
 	if (conn->parent == NULL)
 		return TRUE;
 
 	if (cond & G_IO_ERR ||
-	    cond & G_IO_HUP) {
+	    cond & G_IO_HUP || error) {
 		gdm_debug ("close_if_needed: Got HUP/ERR on %d", conn->fd);
 		conn->source = 0;
 		gdm_connection_close (conn);
@@ -103,11 +103,11 @@ gdm_connection_handler (GIOChannel *source,
 	size_t len;
 
 	if ( ! (cond & G_IO_IN)) 
-		return close_if_needed (conn, cond);
+		return close_if_needed (conn, cond, FALSE);
 
 	VE_IGNORE_EINTR (len = read (conn->fd, buf, sizeof (buf) -1));
 	if (len <= 0) {
-		return close_if_needed (conn, cond);
+		return close_if_needed (conn, cond, TRUE);
 	}
 
 	buf[len] = '\0';
@@ -141,7 +141,7 @@ gdm_connection_handler (GIOChannel *source,
 		}
 	}
 
-	return close_if_needed (conn, cond);
+	return close_if_needed (conn, cond, FALSE);
 }
 
 gboolean
