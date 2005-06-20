0b32cd6fa605bf4fe47da18f88cba0c907e3a64e
Brian Cameron <brian.cameron@sun.com>
2005-06-20 15:23:58 +0000

    On 04/25, a change was made to move seteuid/setegid to before checking the
    2005-06-20  Brian Cameron  <brian.cameron@sun.com>
    
            * daemon/slave.c: On 04/25, a change was made
              to move seteuid/setegid to before checking
              the existance of the user's $HOME id to fix
              bug #301821.  That patch added new seteuid/setegid
              calls before the check, but didn't remove the
              calls after the check.  Calling these functions
              twice is needless and was causing problems on
              NetBSD (refer to bug #301821), so now removing
              the needless second call.  This fixes bug
              #301821.

--- daemon/slave.c
+++ daemon/slave.c
@@ -4058,14 +4058,6 @@ gdm_slave_session_start (void)
 	    home_dir = pwent->pw_dir;
     }
 
-    if G_UNLIKELY (setegid (pwent->pw_gid) != 0 ||
-		   seteuid (pwent->pw_uid) != 0) {
-	    gdm_error ("Cannot set effective user/group id");
-	    gdm_verify_cleanup (d);
-	    session_started = FALSE;
-	    return;
-    }
-
     if G_LIKELY (home_dir_ok) {
 	    /* Sanity check on ~user/.dmrc */
 	    usrcfgok = gdm_file_check ("gdm_slave_session_start", pwent->pw_uid,
