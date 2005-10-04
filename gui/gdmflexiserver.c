41bf8dc3de7f8e536cdbbdca1bc00c142ff195a2
Brian Cameron <Brian.Cameron@sun.com>
2005-10-04 20:29:24 +0000

    Correct reading of config file. Fixes bug #313701. Patch provided by
    2005-10-04  Brian Cameron <Brian.Cameron@sun.com>
    
            * gui/gdmflexiserver.c: Correct reading of
              config file.  Fixes bug #313701.  Patch
              provided by Christiaan Welvaart
              <cjw@daneel.dyndns.org>.

--- gui/gdmflexiserver.c
+++ gui/gdmflexiserver.c
@@ -547,7 +547,7 @@ read_servers (gchar *config_file)
 			char *section;
 			GdmXServer *svr;
 
-			section = g_strdup_printf ("=%s=/", config_file);
+			section = g_strdup_printf ("=%s=/%s/", config_file, k);
 			gnome_config_push_prefix (section);
 
 			if ( ! gnome_config_get_bool
