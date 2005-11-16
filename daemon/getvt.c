2f1cbf79d3e3ed10e82f944a210316fb6df7cc5f
Brian Cameron <Brian.Cameron@sun.com>
2005-11-16 19:33:53 +0000

    Fix #ifdef's.
    2005-11-16  Brian Cameron <Brian.Cameron@sun.com>
    
            * daemon/getvt.c: Fix #ifdef's.

--- daemon/getvt.c
+++ daemon/getvt.c
@@ -1,3 +1,21 @@
+/* GDM - The GNOME Display Manager
+ * Copyright (C) 1998, 1999, 2000 Martin K. Petersen <mkp@mkp.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #include "config.h"
 
 #include <vicious.h>
@@ -65,6 +83,8 @@ open_vt (int vtno)
 	return fd;
 }
 
+#if defined (__linux__)
+
 static int 
 get_free_vt_linux (int *vtfd)
 {
@@ -106,6 +126,8 @@ get_free_vt_linux (int *vtfd)
 	*vtfd = fdv;
 	return vtno;
 }
+
+#elif defined (__FreeBSD__) || defined(__DragonFly__)
 
 static int
 get_free_vt_freebsd_dragonfly (int *vtfd)
@@ -168,6 +190,8 @@ cleanup:
 	}
 	return vtno;
 }
+
+#endif
 
 char *
 gdm_get_empty_vt_argument (int *fd, int *vt)
