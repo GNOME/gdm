6d0c4c19c07d522b5692b8365057c1b478be4794
Brian Cameron <brian.cameron@sun.com>
2006-05-01 19:47:04 +0000

    Updated by running 'cd config && cat XKeepsCrashing
    2006-05-01  Brian Cameron  <brian.cameron@sun.com>
    
            * config/gettextfoo.h, config/XKeepsCrashing:  Updated by running
              'cd config && cat XKeepsCrashing Xsession.in|./extract-shell.sh
              > gettextfoo.h', so the file is regenerated to have the latest
              strings.  Fixes bug #340017.

--- config/gettextfoo.h
+++ config/gettextfoo.h
@@ -1,14 +1,16 @@
-/* DON\'T CHANGE THIS BY HAND! CHANGE THE SCRIPT THIS IS GENERATED */
+/* DO NOT CHANGE HEADER FILE BY HAND! CHANGE THE extract-shell.sh */
+/* SCRIPT THIS IS GENERATED.  ADD A CHANGELOG ENTRY IF YOU MODIFY */
+/* THIS SCRIPT.                                                   */
 /* ALWAYS ADD A CHANGELOG OR I WILL PERSONALLY KICK YOUR ASS! */
-const char *foo = N_("I cannot start the X server (your graphical interface).  It is likely that it is not set up correctly.  You will need to log in on a console and rerun the X configuration program.  Then restart GDM.");
-const char *foo = N_("Would you like me to try to run the X configuration program?  Note that you will need the root password for this.");
+const char *foo = N_("Failed to start the X server (your graphical interface).  It is likely that it is not set up correctly.  You will need to log in on a console and reconfigure the X server.  Then restart GDM.");
+const char *foo = N_("Would you like to try to configure the X server?  Note that you will need the root password for this.");
 const char *foo = N_("Please type in the root (privileged user) password.");
-const char *foo = N_("I will now try to restart the X server again.");
-const char *foo = N_("I will disable this X server for now.  Restart GDM when it is configured correctly.");
-const char *foo = N_("I cannot start the X server (your graphical interface).  It is likely that it is not set up correctly.  Would you like to view the X server output to diagnose the problem?");
+const char *foo = N_("Trying to restart the X server.");
+const char *foo = N_("The X server is now disabled.  Restart GDM when it is configured correctly.");
+const char *foo = N_("Failed to start the X server (your graphical interface).  It is likely that it is not set up correctly.  Would you like to view the X server output to diagnose the problem?");
 const char *foo = N_("Would you like to view the detailed X server output as well?");
-const char *foo = N_("I cannot start the X server (your graphical interface).  It seems that the pointer device (your mouse) is not set up correctly.  Would you like to view the X server output to diagnose the problem?");
-const char *foo = N_("Would you like me to try to run the mouse configuration program?  Note that you will need the root password for this.");
-const char *foo = N_("Would you like me to try to run the mouse configuration program?  Note that you will need the root password for this.");
+const char *foo = N_("Failed to start the X server (your graphical interface).  It seems that the pointer device (your mouse) is not set up correctly.  Would you like to view the X server output to diagnose the problem?");
+const char *foo = N_("Would you like to try to configure the mouse?  Note that you will need the root password for this.");
+const char *foo = N_("Would you like to try to configure the mouse?  Note that you will need the root password for this.");
 const char *foo = N_("This is the failsafe xterm session.  Windows now have focus only if you have your cursor above them.  To get out of this mode type 'exit' in the window in the upper left corner");
-const char *foo = N_("I could not start your session and so I have started the failsafe xterm session.  Windows now have focus only if you have your cursor above them.  To get out of this mode type 'exit' in the window in the upper left corner");
+const char *foo = N_("Failed to start the session, so starting a failsafe xterm session.  Windows will have focus only if the mouse pointer is above them.  To get out of this mode type 'exit' in the window in the upper left corner");
