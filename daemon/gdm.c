a7531ee2060a4b5d97d3ce3bf487b3f673ac9e4b
Brian Cameron <brian.cameron@sun.com>
2006-09-11 20:34:30 +0000

    Move call to get GDM_KEY_SYSTEM_MENU until after authentication check.
    2006-09-11  Brian Cameron  <brian.cameron@sun.com>
    
            * daemon/gdm.c: Move call to get GDM_KEY_SYSTEM_MENU
              until after authentication check.  Fixes bug
              #352838.  Patch by Frederic Crozat
              <fcrozat@mandriva.com>.
            * docs/C/gdm.xml: Make gdmdynamic section a bit more
              clear.

--- daemon/gdm.c
+++ daemon/gdm.c
@@ -3275,7 +3275,6 @@ gdm_handle_user_message (GdmConnection *conn, const gchar *msg, gpointer data)
 		gboolean sysmenu;
 
 		disp = gdm_connection_get_display (conn);
-		sysmenu = gdm_get_value_bool_per_display (disp->name, GDM_KEY_SYSTEM_MENU);
 
 		/* Only allow locally authenticated connections */
 		if ( ! GDM_CONN_AUTHENTICATED(conn) ||
@@ -3288,6 +3287,7 @@ gdm_handle_user_message (GdmConnection *conn, const gchar *msg, gpointer data)
 			return;
 		}
 
+		sysmenu = gdm_get_value_bool_per_display (disp->name, GDM_KEY_SYSTEM_MENU);
 		if (strcmp (action, GDM_SUP_LOGOUT_ACTION_NONE) == 0) {
 			disp->logout_action = GDM_LOGOUT_ACTION_NONE;
 			was_ok = TRUE;
@@ -3328,7 +3328,6 @@ gdm_handle_user_message (GdmConnection *conn, const gchar *msg, gpointer data)
 		gboolean sysmenu;
 
 		disp = gdm_connection_get_display (conn);
-		sysmenu = gdm_get_value_bool_per_display (disp->name, GDM_KEY_SYSTEM_MENU);
 
 		/* Only allow locally authenticated connections */
 		if ( ! GDM_CONN_AUTHENTICATED(conn) ||
@@ -3341,6 +3340,7 @@ gdm_handle_user_message (GdmConnection *conn, const gchar *msg, gpointer data)
 			return;
 		}
 
+		sysmenu = gdm_get_value_bool_per_display (disp->name, GDM_KEY_SYSTEM_MENU);
 		if (strcmp (action, GDM_SUP_LOGOUT_ACTION_NONE) == 0) {
 			safe_logout_action = GDM_LOGOUT_ACTION_NONE;
 			was_ok = TRUE;
