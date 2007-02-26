46e155a6e0e515034d76173686beb032b3a64cfc
Brian Cameron <brian.cameron@sun.com>
2007-02-26 05:24:18 +0000

    Change Session mnemonic to "e" instead of "s" so it doesn't conflict to
    2007-02-26  Brian Cameron  <brian.cameron@sun.com>
    
            * gui/gdmlogin.c: Change Session mnemonic to "e" instead of "s"
              so it doesn't conflict to "Start Again".
    
    svn path=/trunk/; revision=4615

--- gui/gdmlogin.c
+++ gui/gdmlogin.c
@@ -2413,7 +2413,7 @@ gdm_login_gui_init (void)
 
     menu = gtk_menu_new ();
     gdm_login_session_init (menu);
-    sessmenu = gtk_menu_item_new_with_mnemonic (_("_Session"));
+    sessmenu = gtk_menu_item_new_with_mnemonic (_("S_ession"));
     gtk_menu_shell_append (GTK_MENU_SHELL (menubar), sessmenu);
     gtk_menu_item_set_submenu (GTK_MENU_ITEM (sessmenu), menu);
     gtk_widget_show (GTK_WIDGET (sessmenu));
