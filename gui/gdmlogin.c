0b7cf9104b4d76477c41b5082a9f8a38a365e2f0
Brian Cameron <brian.cameron@sun.com>
2007-06-04 04:38:44 +0000

    Set invisible character to '*' if the configuration file doesn't specify
    2007-06-04  Brian Cameron  <brian.cameron@sun.com>
    
            * gui/gdmlogin.c: Set invisible character to '*' if the
              configuration file doesn't specify to use invisible or
              cirlces.  Fixes bug #439089.  Patch by Sebastien Bacher
              <seb128@debian.org>.
    
    svn path=/trunk/; revision=4954

--- gui/gdmlogin.c
+++ gui/gdmlogin.c
@@ -2579,10 +2579,14 @@ gdm_login_gui_init (void)
      */
     g_signal_connect (G_OBJECT (entry), "key_release_event",
 		      G_CALLBACK (key_release_event), NULL);
+
     if (gdm_config_get_bool (GDM_KEY_ENTRY_INVISIBLE))
 	    gtk_entry_set_invisible_char (GTK_ENTRY (entry), 0);
     else if (gdm_config_get_bool (GDM_KEY_ENTRY_CIRCLES))
 	    gtk_entry_set_invisible_char (GTK_ENTRY (entry), 0x25cf);
+    else
+	    gtk_entry_set_invisible_char (GTK_ENTRY (entry), '*');
+
     gtk_entry_set_max_length (GTK_ENTRY (entry), PW_ENTRY_SIZE);
     gtk_widget_set_size_request (entry, 250, -1);
     gtk_widget_ref (entry);
