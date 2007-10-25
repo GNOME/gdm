f7eb5aa0a07bdd0c716a86e311e7215f7c7b2b56
William Jon McCann <mccann@jhu.edu>
2007-10-25 14:42:27 +0000

    Add a stub for settings-daemon.
    2007-10-25  William Jon McCann  <mccann@jhu.edu>
    
    	* configure.ac:
    	* gui/Makefile.am:
    	* gui/settings-daemon/Makefile.am:
    	* gui/settings-daemon/main.c: (main):
    	* gui/simple-chooser/Makefile.am:
    	Add a stub for settings-daemon.
    
    
    svn path=/trunk/; revision=5410

---
+++ gui/settings-daemon/main.c
@@ -0,0 +1,48 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2007 William Jon McCann <mccann@jhu.edu>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "config.h"
+
+#include <stdlib.h>
+#include <libintl.h>
+#include <locale.h>
+
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
+
+#include "gdm-common.h"
+
+int
+main (int argc, char *argv[])
+{
+
+        bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+        bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+        textdomain (GETTEXT_PACKAGE);
+
+        setlocale (LC_ALL, "");
+
+        g_type_init ();
+
+        gtk_init (&argc, &argv);
+
+        gtk_main ();
+
+        return 0;
+}
