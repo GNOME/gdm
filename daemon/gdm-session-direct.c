f07700a954811f20ab77dcaf4cea3244e6a9412d
Ray Strode <rstrode@redhat.com>
2008-02-22 04:37:57 +0000

    remove stale TODO items from top of file
    2008-02-21  Ray Strode  <rstrode@redhat.com>
    
    	* daemon/gdm-session-direct.c:
    	remove stale TODO items from top of file
    
    svn path=/trunk/; revision=5839

--- daemon/gdm-session-direct.c
+++ daemon/gdm-session-direct.c
@@ -17,10 +17,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  * 02111-1307, USA.
- *
- * TODO:                - close should be nicer and shutdown
- *                        pam etc
- *                      - audit libs (linux and solaris) support
  */
 
 #include "config.h"
