194d32de03b59b07c7f59bf526a7e42e650b83f4
William Jon McCann <jmccann@redhat.com>
2008-03-17 16:34:29 +0000

    Set an environment variable to disable fuse mounts in gvfs. From: Matthias
    2008-03-17  William Jon McCann  <jmccann@redhat.com>
    
    	* daemon/gdm-welcome-session.c (get_welcome_environment):
    	Set an environment variable to disable fuse mounts in gvfs.
    	From: Matthias Clasen <mclasen@redhat.com>
    
    
    svn path=/trunk/; revision=6030

--- daemon/gdm-welcome-session.c
+++ daemon/gdm-welcome-session.c
@@ -316,6 +316,7 @@ get_welcome_environment (GdmWelcomeSession *welcome_session)
         g_hash_table_insert (hash, g_strdup ("PATH"), g_strdup (g_getenv ("PATH")));
 
         g_hash_table_insert (hash, g_strdup ("RUNNING_UNDER_GDM"), g_strdup ("true"));
+        g_hash_table_insert (hash, g_strdup ("GVFS_DISABLE_FUSE"), g_strdup ("1"));
 
         g_hash_table_foreach (hash, (GHFunc)listify_hash, env);
         g_hash_table_destroy (hash);
