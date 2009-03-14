bce5708585ba697195ea411e0a751f5da6c0864c
Brian Cameron <brian.cameron@sun.com>
2009-03-14 01:06:00 +0000

    Call gdm_settings_direct_init. Otherwise when these programs try to read
    2009-03-13  Brian Cameron  <brian.cameron@sun.com>
    
            * daemon/xdmcp-chooser-slave-main.c, daemon/simple-slave-main.c:
              Call gdm_settings_direct_init.  Otherwise when these programs try
              to read the xdmcp/PingIntervalSeconds configuration value, they
              fail to load it.
    
    svn path=/trunk/; revision=6769

--- daemon/simple-slave-main.c
+++ daemon/simple-slave-main.c
@@ -43,10 +43,13 @@
 #include "gdm-log.h"
 #include "gdm-common.h"
 #include "gdm-simple-slave.h"
+#include "gdm-settings.h"
+#include "gdm-settings-direct.h"
 
 #include "gdm-settings-client.h"
 
-static int gdm_return_code = 0;
+static GdmSettings     *settings        = NULL;
+static int              gdm_return_code = 0;
 
 static DBusGConnection *
 get_system_bus (void)
@@ -201,6 +204,17 @@ main (int    argc,
         }
 
         gdm_log_init ();
+
+        settings = gdm_settings_new ();
+        if (settings == NULL) {
+                g_warning ("Unable to initialize settings");
+                goto out;
+        }
+
+        if (! gdm_settings_direct_init (settings, GDMCONFDIR "/gdm.schemas", "/")) {
+                g_warning ("Unable to initialize settings");
+                goto out;
+        }
 
         gdm_log_set_debug (is_debug_set (debug));
 
--- daemon/xdmcp-chooser-slave-main.c
+++ daemon/xdmcp-chooser-slave-main.c
@@ -43,8 +43,11 @@
 #include "gdm-log.h"
 #include "gdm-common.h"
 #include "gdm-xdmcp-chooser-slave.h"
+#include "gdm-settings.h"
+#include "gdm-settings-direct.h"
 
-static int gdm_return_code = 0;
+static GdmSettings     *settings        = NULL;
+static int              gdm_return_code = 0;
 
 static DBusGConnection *
 get_system_bus (void)
@@ -198,6 +201,17 @@ main (int    argc,
         }
 
         gdm_log_init ();
+
+        settings = gdm_settings_new ();
+        if (settings == NULL) {
+                g_warning ("Unable to initialize settings");
+                goto out;
+        }
+
+        if (! gdm_settings_direct_init (settings, GDMCONFDIR "/gdm.schemas", "/")) {
+                g_warning ("Unable to initialize settings");
+                goto out;
+        }
 
         gdm_log_set_debug (is_debug_set (debug));
 
