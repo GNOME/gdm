a05e3d537e186b794042c55fa8d56281da640570
Ray Strode <rstrode@redhat.com>
2010-03-15 23:03:09 -0400

    drop loading-users signal
    No one listens for it, so there's no point in having it.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=610179

--- gui/simple-greeter/gdm-user-manager.h
+++ gui/simple-greeter/gdm-user-manager.h
@@ -46,7 +46,6 @@ typedef struct
 {
         GObjectClass   parent_class;
 
-        void          (* loading_users)             (GdmUserManager *user_manager);
         void          (* users_loaded)              (GdmUserManager *user_manager);
         void          (* user_added)                (GdmUserManager *user_manager,
                                                      GdmUser        *user);
