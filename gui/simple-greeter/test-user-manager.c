f8fb7c12bc2299c1bc7084cc4a7b93f2780386ff
William Jon McCann <jmccann@redhat.com>
2010-06-09 22:08:26 -0400

    Don't translate strings in test program

--- gui/simple-greeter/test-user-manager.c
+++ gui/simple-greeter/test-user-manager.c
@@ -97,7 +97,7 @@ main (int argc, char *argv[])
 
         g_type_init ();
 
-        context = g_option_context_new (_("GNOME Display Manager"));
+        context = g_option_context_new ("GNOME Display Manager");
         g_option_context_add_main_entries (context, entries, NULL);
         g_option_context_set_ignore_unknown_options (context, TRUE);
 
