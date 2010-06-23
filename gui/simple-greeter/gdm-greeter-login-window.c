178153f74bcef9ac8983cb3cbe58057a6c8ed94b
William Jon McCann <jmccann@redhat.com>
2010-06-23 13:33:30 -0400

    Don't jump to authenticate before users are loaded
    cherry picked from afbacf9323a5564e01c86bd30739f3918da17915

--- gui/simple-greeter/gdm-greeter-login-window.c
+++ gui/simple-greeter/gdm-greeter-login-window.c
@@ -607,7 +607,9 @@ can_jump_to_authenticate (GdmGreeterLoginWindow *login_window)
 {
         gboolean res;
 
-        if (login_window->priv->user_list_disabled) {
+        if (!login_window->priv->user_chooser_loaded) {
+                res = FALSE;
+        } else if (login_window->priv->user_list_disabled) {
                 res = (login_window->priv->timed_login_username == NULL);
         } else {
                 res = user_chooser_has_no_user (login_window);
@@ -704,7 +706,7 @@ gdm_greeter_login_window_ready (GdmGreeterLoginWindow *login_window)
                 retry_login (login_window);
         } else {
                 /* If the user list is disabled, then start the PAM conversation */
-                if (login_window->priv->user_list_disabled || user_chooser_has_no_user (login_window)) {
+                if (can_jump_to_authenticate (login_window)) {
                         g_debug ("Starting PAM conversation since user list disabled");
                         g_signal_emit (G_OBJECT (login_window), signals[USER_SELECTED],
                                        0, GDM_USER_CHOOSER_USER_OTHER);
