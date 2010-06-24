b01744d01a859edc5d1a6c38e5adff7856a79919
Ray Strode <rstrode@redhat.com>
2010-06-24 19:41:02 -0400

    Drop GCancellable from user manager struct
    It was unused.

--- gui/simple-greeter/gdm-user-manager.c
+++ gui/simple-greeter/gdm-user-manager.c
@@ -144,7 +144,6 @@ struct GdmUserManagerPrivate
         gboolean               include_all;
 
         gboolean               load_passwd_pending;
-        GCancellable          *cancellable;
 
         guint                  load_id;
         guint                  reload_passwd_id;
