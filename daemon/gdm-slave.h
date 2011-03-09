90ab791544d676629800e80fcf7a28a628f94e94
Ray Strode <rstrode@redhat.com>
2011-03-09 14:13:36 -0500

    daemon: warp pointer to convenient place at startup
    This makes sure we always start the greeter window in a consistent
    place, and it makes sure the pointer doesn't start up on top of
    the greeter window.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=644327

--- daemon/gdm-slave.h
+++ daemon/gdm-slave.h
@@ -73,6 +73,9 @@ gboolean            gdm_slave_switch_to_user_session (GdmSlave   *slave,
                                                       const char *username);
 
 gboolean            gdm_slave_connect_to_x11_display (GdmSlave   *slave);
+
+void                gdm_slave_set_initial_cursor_position (GdmSlave *slave);
+
 void                gdm_slave_set_busy_cursor        (GdmSlave   *slave);
 gboolean            gdm_slave_run_script             (GdmSlave   *slave,
                                                       const char *dir,
