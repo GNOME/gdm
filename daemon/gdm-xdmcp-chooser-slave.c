GNOME/gdm

commit be67db1c11868ea2788cece74bbe53e76522292c

daemon: Remove GdmGreeterSession and GdmChooserSession
With the recent cleanup, these two are just dummy class subtypes.
Duplicating this one more time for the setup session wouldn't be
worth it. Into the trash it goes.

https://bugzilla.gnome.org/show_bug.cgi?id=678057

daemon/gdm-xdmcp-chooser-slave.c
@@ -42,7 +42,7 @@
 #include "gdm-xdmcp-chooser-slave-glue.h"
 
 #include "gdm-server.h"
-#include "gdm-chooser-session.h"
+#include "gdm-welcome-session.h"
 #include "gdm-settings-direct.h"
 #include "gdm-settings-keys.h"
 #include "gdm-session.h"
@@ -64,7 +64,7 @@ struct GdmXdmcpChooserSlavePrivate
 
         guint              connection_attempts;
 
-        GdmChooserSession *chooser;
+        GdmWelcomeSession *chooser;
 
         GdmDBusXdmcpChooserSlave *skeleton;
 };
@@ -77,7 +77,7 @@ G_DEFINE_TYPE (GdmXdmcpChooserSlave, gdm_xdmcp_chooser_slave, GDM_TYPE_SLAVE)
 
 
 static void
-on_chooser_session_opened (GdmChooserSession    *chooser,
+on_chooser_session_opened (GdmWelcomeSession    *chooser,
                            GdmXdmcpChooserSlave *slave)
 {
         char       *session_id;
@@ -90,14 +90,14 @@ on_chooser_session_opened (GdmChooserSession    *chooser,
 }
 
 static void
-on_chooser_session_start (GdmChooserSession    *chooser,
+on_chooser_session_start (GdmWelcomeSession    *chooser,
                           GdmXdmcpChooserSlave *slave)
 {
         g_debug ("GdmXdmcpChooserSlave: Chooser started");
 }
 
 static void
-on_chooser_session_stop (GdmChooserSession    *chooser,
+on_chooser_session_stop (GdmWelcomeSession    *chooser,
                          GdmXdmcpChooserSlave *slave)
 {
         g_debug ("GdmXdmcpChooserSlave: Chooser stopped");
@@ -108,7 +108,7 @@ on_chooser_session_stop (GdmChooserSession    *chooser,
 }
 
 static void
-on_chooser_session_exited (GdmChooserSession    *chooser,
+on_chooser_session_exited (GdmWelcomeSession    *chooser,
                            int                   code,
                            GdmXdmcpChooserSlave *slave)
 {
@@ -120,7 +120,7 @@ on_chooser_session_exited (GdmChooserSession    *chooser,
 }
 
 static void
-on_chooser_session_died (GdmChooserSession    *chooser,
+on_chooser_session_died (GdmWelcomeSession    *chooser,
                          int                   signal,
                          GdmXdmcpChooserSlave *slave)
 {
@@ -167,6 +167,20 @@ setup_server (GdmXdmcpChooserSlave *slave)
 {
         /* Set the busy cursor */
         gdm_slave_set_busy_cursor (GDM_SLAVE (slave));
+}
+
+static GdmWelcomeSession *
+create_chooser_session (const char *display_name,
+                        const char *display_device,
+                        const char *display_hostname)
+{
+        return g_object_new (GDM_TYPE_WELCOME_SESSION,
+                             "command", LIBEXECDIR "/gdm-simple-chooser",
+                             "verification-mode", GDM_SESSION_VERIFICATION_MODE_CHOOSER,
+                             "x11-display-name", display_name,
+                             "x11-display-device", display_device,
+                             "x11-display-hostname", display_hostname,
+                             NULL);
 }
 
 static void

daemon/Makefile.am
@@ -144,8 +144,6 @@ gdm_simple_slave_SOURCES = 		\
 	simple-slave-main.c 		\
 	gdm-welcome-session.c		\
 	gdm-welcome-session.h		\
-	gdm-greeter-session.c		\
-	gdm-greeter-session.h		\
 	gdm-server.c			\
 	gdm-server.h			\
 	gdm-session.c			\
@@ -198,8 +196,6 @@ gdm_xdmcp_chooser_slave_SOURCES = 		\
 	gdm-session-worker-job.h		\
 	gdm-welcome-session.c			\
 	gdm-welcome-session.h			\
-	gdm-chooser-session.c			\
-	gdm-chooser-session.h			\
 	gdm-xerrors.h				\
 	gdm-xerrors.c				\
 	gdm-slave.c				\

daemon/gdm-chooser-session.c
@@ -1,89 +1,0 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
- *
- * Copyright (C) 2008 William Jon McCann <jmccann@redhat.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- */
-
-#include "config.h"
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <ctype.h>
-#include <pwd.h>
-#include <grp.h>
-#include <signal.h>
-
-#include <glib.h>
-#include <glib/gi18n.h>
-#include <glib-object.h>
-
-#include "gdm-common.h"
-
-#include "gdm-session.h"
-#include "gdm-welcome-session.h"
-#include "gdm-chooser-session.h"
-
-#define GDM_CHOOSER_SERVER_DBUS_PATH      "/org/gnome/DisplayManager/ChooserServer"
-#define GDM_CHOOSER_SERVER_DBUS_INTERFACE "org.gnome.DisplayManager.ChooserServer"
-
-#define GDM_CHOOSER_SESSION_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), GDM_TYPE_CHOOSER_SESSION, GdmChooserSessionPrivate))
-
-struct GdmChooserSessionPrivate
-{
-        gpointer dummy;
-};
-
-static void     gdm_chooser_session_class_init    (GdmChooserSessionClass *klass);
-static void     gdm_chooser_session_init          (GdmChooserSession      *chooser_session);
-
-G_DEFINE_TYPE (GdmChooserSession, gdm_chooser_session, GDM_TYPE_WELCOME_SESSION)
-
-static void
-gdm_chooser_session_class_init (GdmChooserSessionClass *klass)
-{
-        g_type_class_add_private (klass, sizeof (GdmChooserSessionPrivate));
-}
-
-static void
-gdm_chooser_session_init (GdmChooserSession *chooser_session)
-{
-        chooser_session->priv = GDM_CHOOSER_SESSION_GET_PRIVATE (chooser_session);
-}
-
-GdmChooserSession *
-gdm_chooser_session_new (const char *display_name,
-                         const char *display_device,
-                         const char *display_hostname)
-{
-        GObject *object;
-
-        object = g_object_new (GDM_TYPE_CHOOSER_SESSION,
-                               "command", LIBEXECDIR "/gdm-simple-chooser",
-                               "verification-mode", GDM_SESSION_VERIFICATION_MODE_CHOOSER,
-                               "x11-display-name", display_name,
-                               "x11-display-device", display_device,
-                               "x11-display-hostname", display_hostname,
-                               NULL);
-
-        return GDM_CHOOSER_SESSION (object);
-}

daemon/gdm-chooser-session.h
@@ -1,58 +1,0 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
- *
- * Copyright (C) 2008 William Jon McCann <jmccann@redhat.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- */
-
-
-#ifndef __GDM_CHOOSER_SESSION_H
-#define __GDM_CHOOSER_SESSION_H
-
-#include <glib-object.h>
-
-#include "gdm-welcome-session.h"
-
-G_BEGIN_DECLS
-
-#define GDM_TYPE_CHOOSER_SESSION         (gdm_chooser_session_get_type ())
-#define GDM_CHOOSER_SESSION(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), GDM_TYPE_CHOOSER_SESSION, GdmChooserSession))
-#define GDM_CHOOSER_SESSION_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), GDM_TYPE_CHOOSER_SESSION, GdmChooserSessionClass))
-#define GDM_IS_CHOOSER_SESSION(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), GDM_TYPE_CHOOSER_SESSION))
-#define GDM_IS_CHOOSER_SESSION_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), GDM_TYPE_CHOOSER_SESSION))
-#define GDM_CHOOSER_SESSION_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), GDM_TYPE_CHOOSER_SESSION, GdmChooserSessionClass))
-
-typedef struct GdmChooserSessionPrivate GdmChooserSessionPrivate;
-
-typedef struct
-{
-        GdmWelcomeSession         parent;
-        GdmChooserSessionPrivate *priv;
-} GdmChooserSession;
-
-typedef struct
-{
-        GdmWelcomeSessionClass    parent_class;
-} GdmChooserSessionClass;
-
-GType                 gdm_chooser_session_get_type           (void);
-GdmChooserSession *   gdm_chooser_session_new                (const char        *display_name,
-                                                              const char        *display_device,
-                                                              const char        *display_hostname);
-
-G_END_DECLS
-
-#endif /* __GDM_CHOOSER_SESSION_H */

daemon/gdm-greeter-session.c
@@ -1,101 +1,0 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
- *
- * Copyright (C) 2007 William Jon McCann <mccann@jhu.edu>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- */
-
-#include "config.h"
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <ctype.h>
-#include <pwd.h>
-#include <grp.h>
-#include <signal.h>
-
-#include <glib.h>
-#include <glib/gi18n.h>
-#include <glib-object.h>
-
-#include "gdm-welcome-session.h"
-#include "gdm-greeter-session.h"
-#include "gdm-settings-direct.h"
-#include "gdm-settings-keys.h"
-
-#define GDM_GREETER_SERVER_DBUS_PATH      "/org/gnome/DisplayManager/GreeterServer"
-#define GDM_GREETER_SERVER_DBUS_INTERFACE "org.gnome.DisplayManager.GreeterServer"
-
-#define GDM_GREETER_SESSION_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), GDM_TYPE_GREETER_SESSION, GdmGreeterSessionPrivate))
-
-struct GdmGreeterSessionPrivate
-{
-        gpointer dummy;
-};
-
-static void     gdm_greeter_session_class_init    (GdmGreeterSessionClass *klass);
-static void     gdm_greeter_session_init  (GdmGreeterSession      *greeter_session);
-
-G_DEFINE_TYPE (GdmGreeterSession, gdm_greeter_session, GDM_TYPE_WELCOME_SESSION)
-
-static void
-gdm_greeter_session_class_init (GdmGreeterSessionClass *klass)
-{
-        g_type_class_add_private (klass, sizeof (GdmGreeterSessionPrivate));
-}
-
-static void
-gdm_greeter_session_init (GdmGreeterSession *greeter_session)
-{
-
-        greeter_session->priv = GDM_GREETER_SESSION_GET_PRIVATE (greeter_session);
-}
-
-GdmGreeterSession *
-gdm_greeter_session_new (const char *display_name,
-                         const char *seat_id,
-                         const char *display_device,
-                         const char *display_hostname,
-                         gboolean    display_is_local)
-{
-        GObject *object;
-        gboolean debug = FALSE;
-        char *command = BINDIR "/gnome-session -f";
-
-        gdm_settings_direct_get_boolean (GDM_KEY_DEBUG, &debug);
-
-        if (debug) {
-                command = BINDIR "/gnome-session -f --debug";
-        }
-
-        object = g_object_new (GDM_TYPE_GREETER_SESSION,
-                               "command", command,
-                               "x11-display-name", display_name,
-                               "x11-display-seat-id", seat_id,
-                               "x11-display-device", display_device,
-                               "x11-display-hostname", display_hostname,
-                               "x11-display-is-local", display_is_local,
-                               "runtime-dir", GDM_SCREENSHOT_DIR,
-                               NULL);
-
-        return GDM_GREETER_SESSION (object);
-}

daemon/gdm-greeter-session.h
@@ -1,60 +1,0 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
- *
- * Copyright (C) 2007 William Jon McCann <mccann@jhu.edu>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- */
-
-
-#ifndef __GDM_GREETER_SESSION_H
-#define __GDM_GREETER_SESSION_H
-
-#include <glib-object.h>
-
-#include "gdm-welcome-session.h"
-
-G_BEGIN_DECLS
-
-#define GDM_TYPE_GREETER_SESSION         (gdm_greeter_session_get_type ())
-#define GDM_GREETER_SESSION(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), GDM_TYPE_GREETER_SESSION, GdmGreeterSession))
-#define GDM_GREETER_SESSION_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), GDM_TYPE_GREETER_SESSION, GdmGreeterSessionClass))
-#define GDM_IS_GREETER_SESSION(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), GDM_TYPE_GREETER_SESSION))
-#define GDM_IS_GREETER_SESSION_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), GDM_TYPE_GREETER_SESSION))
-#define GDM_GREETER_SESSION_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), GDM_TYPE_GREETER_SESSION, GdmGreeterSessionClass))
-
-typedef struct GdmGreeterSessionPrivate GdmGreeterSessionPrivate;
-
-typedef struct
-{
-        GdmWelcomeSession         parent;
-        GdmGreeterSessionPrivate *priv;
-} GdmGreeterSession;
-
-typedef struct
-{
-        GdmWelcomeSessionClass    parent_class;
-} GdmGreeterSessionClass;
-
-GType                 gdm_greeter_session_get_type           (void);
-GdmGreeterSession *   gdm_greeter_session_new                (const char        *display_name,
-                                                              const char        *seat_id,
-                                                              const char        *display_device,
-                                                              const char        *display_hostname,
-                                                              gboolean           display_is_local);
-
-G_END_DECLS
-
-#endif /* __GDM_GREETER_SESSION_H */

daemon/gdm-simple-slave.c
@@ -50,7 +50,7 @@
 #include "gdm-server.h"
 #include "gdm-session.h"
 #include "gdm-session-glue.h"
-#include "gdm-greeter-session.h"
+#include "gdm-welcome-session.h"
 #include "gdm-settings-direct.h"
 #include "gdm-settings-keys.h"
 
@@ -77,7 +77,7 @@ struct GdmSimpleSlavePrivate
 
         GdmServer         *server;
         GdmSession        *session;
-        GdmGreeterSession *greeter;
+        GdmWelcomeSession *greeter;
 
         GHashTable        *open_reauthentication_requests;
 
@@ -690,7 +690,7 @@ create_new_session (GdmSimpleSlave  *slave)
 }
 
 static void
-on_greeter_session_opened (GdmGreeterSession *greeter,
+on_greeter_session_opened (GdmWelcomeSession *greeter,
                            GdmSimpleSlave    *slave)
 {
         char       *session_id;
@@ -703,14 +703,14 @@ on_greeter_session_opened (GdmGreeterSession *greeter,
 }
 
 static void
-on_greeter_session_started (GdmGreeterSession *greeter,
+on_greeter_session_started (GdmWelcomeSession *greeter,
                             GdmSimpleSlave    *slave)
 {
         g_debug ("GdmSimpleSlave: Greeter started");
 }
 
 static void
-on_greeter_session_stopped (GdmGreeterSession *greeter,
+on_greeter_session_stopped (GdmWelcomeSession *greeter,
                             GdmSimpleSlave    *slave)
 {
         g_debug ("GdmSimpleSlave: Greeter stopped");
@@ -726,7 +726,7 @@ on_greeter_session_stopped (GdmGreeterSession *greeter,
 }
 
 static void
-on_greeter_session_exited (GdmGreeterSession    *greeter,
+on_greeter_session_exited (GdmWelcomeSession    *greeter,
                            int                   code,
                            GdmSimpleSlave       *slave)
 {
@@ -737,7 +737,7 @@ on_greeter_session_exited (GdmGreeterSession    *greeter,
 }
 
 static void
-on_greeter_session_died (GdmGreeterSession    *greeter,
+on_greeter_session_died (GdmWelcomeSession    *greeter,
                          int                   signal,
                          GdmSimpleSlave       *slave)
 {
@@ -842,6 +842,33 @@ setup_server (GdmSimpleSlave *slave)
 #endif
 }
 
+static GdmWelcomeSession *
+create_greeter_session (const char *display_name,
+                        const char *seat_id,
+                        const char *display_device,
+                        const char *display_hostname,
+                        gboolean    display_is_local)
+{
+        gboolean debug = FALSE;
+        char *command = BINDIR "/gnome-session -f";
+
+        gdm_settings_direct_get_boolean (GDM_KEY_DEBUG, &debug);
+
+        if (debug) {
+                command = BINDIR "/gnome-session -f --debug";
+        }
+
+        return g_object_new (GDM_TYPE_WELCOME_SESSION,
+                             "command", command,
+                             "x11-display-name", display_name,
+                             "x11-display-seat-id", seat_id,
+                             "x11-display-device", display_device,
+                             "x11-display-hostname", display_hostname,
+                             "x11-display-is-local", display_is_local,
+                             "runtime-dir", GDM_SCREENSHOT_DIR,
+                             NULL);
+}
+
 static void
 start_greeter (GdmSimpleSlave *slave)
 {
@@ -894,11 +921,11 @@ start_greeter (GdmSimpleSlave *slave)
         gdm_slave_run_script (GDM_SLAVE (slave), GDMCONFDIR "/Init", GDM_USERNAME);
 
         g_debug ("GdmSimpleSlave: Creating greeter on %s %s %s", display_name, display_device, display_hostname);
-        slave->priv->greeter = gdm_greeter_session_new (display_name,
-                                                        seat_id,
-                                                        display_device,
-                                                        display_hostname,
-                                                        display_is_local);
+        slave->priv->greeter = create_greeter_session (display_name,
+                                                       seat_id,
+                                                       display_device,
+                                                       display_hostname,
+                                                       display_is_local);
         g_signal_connect (slave->priv->greeter,
                           "opened",
                           G_CALLBACK (on_greeter_session_opened),

commit c45ac3ad9f8650ea35ba9c04ebbec0ec77236216

daemon: fix chooser slave
commit be67db1c11868ea2788cece74bbe53e76522292c broke the build
because of a tiny think-o in the code.

daemon/gdm-xdmcp-chooser-slave.c
@@ -226,9 +226,9 @@ run_chooser (GdmXdmcpChooserSlave *slave)
         gdm_slave_run_script (GDM_SLAVE (slave), GDMCONFDIR "/Init", GDM_USERNAME);
 
         g_debug ("GdmXdmcpChooserSlave: Creating chooser on %s %s %s", display_name, display_device, display_hostname);
-        slave->priv->chooser = gdm_chooser_session_new (display_name,
-                                                        display_device,
-                                                        display_hostname);
+        slave->priv->chooser = create_chooser_session (display_name,
+                                                       display_device,
+                                                       display_hostname);
         g_signal_connect (slave->priv->chooser,
                           "opened",
                           G_CALLBACK (on_chooser_session_opened),