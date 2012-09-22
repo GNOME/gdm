eda8eede28cbadfb4d1c2a6c09ad06da26c1d8f9
Matthias Clasen <mclasen@redhat.com>
2012-09-22 15:43:55 -0400

    Don't run gnome-session in failsafe mode
    Explicitly pass /usr/share/gdm/greeter/autostart
    as autostart dir, and don't run gnome-session in failsafe
    mode, so it can autostart applications from that directory.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=684474

--- daemon/gdm-simple-slave.c
+++ daemon/gdm-simple-slave.c
@@ -982,7 +982,10 @@ create_environment (const char *session_id,
         gdm_settings_direct_get_boolean (GDM_KEY_DEBUG, &debug);
 
         args = g_ptr_array_new ();
-        g_ptr_array_add (args, BINDIR "/gnome-session -f");
+        g_ptr_array_add (args, BINDIR "/gnome-session");
+
+        g_ptr_array_add (args, "--autostart");
+        g_ptr_array_add (args, DATADIR "/gdm/greeter/autostart");
 
         if (debug) {
                 g_ptr_array_add (args, "--debug");
