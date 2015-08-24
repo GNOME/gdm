GNOME/gdm

commit cab736cfb32aa25677287ff261ae1cb11f036e89

session: load locale.conf from gdm-session.c
Right now we're not picking up changes to locale.conf completely
at runtime.

This commit moves reading locale.conf to a different place in the
code, so that it's more effectively read and used.

https://bugzilla.gnome.org/show_bug.cgi?id=751865

daemon/gdm-session.c
@@ -485,8 +485,17 @@ get_session_command_for_name (const char *name,
 static const char *
 get_default_language_name (GdmSession *self)
 {
+    const char *default_language;
+
     if (self->priv->saved_language != NULL) {
-                return self->priv->saved_language;
+            return self->priv->saved_language;
+    }
+
+    default_language = g_hash_table_lookup (self->priv->environment,
+                                            "LANG");
+
+    if (default_language != NULL) {
+            return default_language;
     }
 
     return setlocale (LC_MESSAGES, NULL);
@@ -1672,6 +1681,90 @@ free_conversation (GdmSessionConversation *conversation)
 }
 
 static void
+load_lang_config_file (GdmSession *self)
+{
+        static const char *config_file = LANG_CONFIG_FILE;
+        gchar         *contents = NULL;
+        gchar         *p;
+        gchar         *key;
+        gchar         *value;
+        gsize          length;
+        GError        *error;
+        GString       *line;
+        GRegex        *re;
+
+        if (!g_file_test (config_file, G_FILE_TEST_EXISTS)) {
+                g_debug ("Cannot access '%s'", config_file);
+                return;
+        }
+
+        error = NULL;
+        if (!g_file_get_contents (config_file, &contents, &length, &error)) {
+                g_debug ("Failed to parse '%s': %s",
+                         LANG_CONFIG_FILE,
+                         (error && error->message) ? error->message : "(null)");
+                g_error_free (error);
+                return;
+        }
+
+        if (!g_utf8_validate (contents, length, NULL)) {
+                g_warning ("Invalid UTF-8 in '%s'", config_file);
+                g_free (contents);
+                return;
+        }
+
+        re = g_regex_new ("(?P<key>(LANG|LANGUAGE|LC_CTYPE|LC_NUMERIC|LC_TIME|LC_COLLATE|LC_MONETARY|LC_MESSAGES|LC_PAPER|LC_NAME|LC_ADDRESS|LC_TELEPHONE|LC_MEASUREMENT|LC_IDENTIFICATION|LC_ALL))=(\")?(?P<value>[^\"]*)?(\")?", 0, 0, &error);
+        if (re == NULL) {
+                g_warning ("Failed to regex: %s",
+                           (error && error->message) ? error->message : "(null)");
+                g_error_free (error);
+                g_free (contents);
+                return;
+        }
+
+        line = g_string_new ("");
+        for (p = contents; p && *p; p = g_utf8_find_next_char (p, NULL)) {
+                gunichar ch;
+                GMatchInfo *match_info = NULL;
+
+                ch = g_utf8_get_char (p);
+                if ((ch != '\n') && (ch != '\0')) {
+                        g_string_append_unichar (line, ch);
+                        continue;
+                }
+
+                if (line->str && g_utf8_get_char (line->str) == '#') {
+                        goto next_line;
+                }
+
+                if (!g_regex_match (re, line->str, 0, &match_info)) {
+                        goto next_line;
+                }
+
+                if (!g_match_info_matches (match_info)) {
+                        goto next_line;
+                }
+
+                key = g_match_info_fetch_named (match_info, "key");
+                value = g_match_info_fetch_named (match_info, "value");
+
+                if (key && *key && value && *value) {
+			g_setenv (key, value, TRUE);
+                }
+
+                g_free (key);
+                g_free (value);
+next_line:
+                g_match_info_free (match_info);
+                g_string_set_size (line, 0);
+        }
+
+        g_string_free (line, TRUE);
+        g_regex_unref (re);
+        g_free (contents);
+}
+
+static void
 gdm_session_init (GdmSession *self)
 {
         self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
@@ -1688,6 +1781,7 @@ gdm_session_init (GdmSession *self)
                                                          (GDestroyNotify) g_free,
                                                          (GDestroyNotify) g_free);
 
+        load_lang_config_file (self);
         setup_worker_server (self);
         setup_outside_server (self);
 }

daemon/gdm-launch-environment.c
@@ -110,109 +110,6 @@ static void     gdm_launch_environment_finalize      (GObject                   
 
 G_DEFINE_TYPE (GdmLaunchEnvironment, gdm_launch_environment, G_TYPE_OBJECT)
 
-static void
-load_lang_config_file (const char  *config_file,
-                       const char **str_array)
-{
-        gchar         *contents = NULL;
-        gchar         *p;
-        gchar         *str_joinv;
-        gchar         *pattern;
-        gchar         *key;
-        gchar         *value;
-        gsize          length;
-        GError        *error;
-        GString       *line;
-        GRegex        *re;
-
-        g_return_if_fail (config_file != NULL);
-        g_return_if_fail (str_array != NULL);
-
-        if (!g_file_test (config_file, G_FILE_TEST_EXISTS)) {
-                g_debug ("Cannot access '%s'", config_file);
-                return;
-        }
-
-        error = NULL;
-        if (!g_file_get_contents (config_file, &contents, &length, &error)) {
-                g_debug ("Failed to parse '%s': %s",
-                         config_file,
-                         (error && error->message) ? error->message : "(null)");
-                g_error_free (error);
-                return;
-        }
-
-        if (!g_utf8_validate (contents, length, NULL)) {
-                g_warning ("Invalid UTF-8 in '%s'", config_file);
-                g_free (contents);
-                return;
-        }
-
-        str_joinv = g_strjoinv ("|", (char **) str_array);
-        if (str_joinv == NULL) {
-                g_warning ("Error in joined");
-                g_free (contents);
-                return;
-        }
-
-        pattern = g_strdup_printf ("(?P<key>(%s))=(\")?(?P<value>[^\"]*)?(\")?",
-                                   str_joinv);
-        error = NULL;
-        re = g_regex_new (pattern, 0, 0, &error);
-        g_free (pattern);
-        g_free (str_joinv);
-        if (re == NULL) {
-                g_warning ("Failed to regex: %s",
-                           (error && error->message) ? error->message : "(null)");
-                g_error_free (error);
-                g_free (contents);
-                return;
-        }
-
-        line = g_string_new ("");
-        for (p = contents; p && *p; p = g_utf8_find_next_char (p, NULL)) {
-                gunichar ch;
-                GMatchInfo *match_info = NULL;
-
-                ch = g_utf8_get_char (p);
-                if ((ch != '\n') && (ch != '\0')) {
-                        g_string_append_unichar (line, ch);
-                        continue;
-                }
-
-                if (line->str && g_utf8_get_char (line->str) == '#') {
-                        goto next_line;
-                }
-
-                if (!g_regex_match (re, line->str, 0, &match_info)) {
-                        goto next_line;
-                }
-
-                if (!g_match_info_matches (match_info)) {
-                        goto next_line;
-                }
-
-                key = g_match_info_fetch_named (match_info, "key");
-                value = g_match_info_fetch_named (match_info, "value");
-
-                if (key && *key && value && *value) {
-                        g_setenv (key, value, TRUE);
-                } else if (key && *key) {
-                        g_unsetenv (key);
-                }
-
-                g_free (key);
-                g_free (value);
-next_line:
-                g_match_info_free (match_info);
-                g_string_set_size (line, 0);
-        }
-
-        g_string_free (line, TRUE);
-        g_regex_unref (re);
-        g_free (contents);
-}
-
 static GHashTable *
 build_launch_environment (GdmLaunchEnvironment *launch_environment,
                           gboolean              start_session)
@@ -228,9 +125,6 @@ build_launch_environment (GdmLaunchEnvironment *launch_environment,
         };
         char *system_data_dirs;
         int i;
-
-        load_lang_config_file (LANG_CONFIG_FILE,
-                               (const char **) optional_environment);
 
         /* create a hash table of current environment, then update keys has necessary */
         hash = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_free);

commit a1fbd8cb1199a9a918e2be212075b6d3d89c0914

session: fix corruption of GDM_LANG environment variable
commit cab736cfb32aa25677287ff261ae1cb11f036e89 introduced a
bug that leads to GDM_LANG getting set to gibberish.  This
causes spew to the log and, in some cases, prevents successful
login.

This commit addresses the problem by duplicating a string that
is, otherwise, freed before its second use.

https://bugzilla.gnome.org/show_bug.cgi?id=754033

daemon/gdm-session.c
@@ -2451,7 +2451,7 @@ set_up_session_environment (GdmSession *self)
 {
         GdmSessionDisplayMode display_mode;
         gchar *desktop_names;
-        const char *locale;
+        char *locale;
 
         gdm_session_set_environment_variable (self,
                                               "GDMSESSION",
@@ -2470,7 +2470,7 @@ set_up_session_environment (GdmSession *self)
 
         set_up_session_language (self);
 
-        locale = get_default_language_name (self);
+        locale = g_strdup (get_default_language_name (self));
 
         if (locale != NULL && locale[0] != '\0') {
                 gdm_session_set_environment_variable (self,
@@ -2480,6 +2480,8 @@ set_up_session_environment (GdmSession *self)
                                                       "GDM_LANG",
                                                       locale);
         }
+
+        g_free (locale);
 
         display_mode = gdm_session_get_display_mode (self);
         if (display_mode == GDM_SESSION_DISPLAY_MODE_REUSE_VT) {