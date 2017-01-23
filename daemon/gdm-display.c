74cd3c0bbf6e984cb52e2734af5bd7a5f9f8cf13
Rui Matos <tiagomatos@gmail.com>
2017-01-23 20:19:51 +0100

    Honor initial setup being disabled by distro installer
    Sysadmins might want to disable any kind of initial setup for their
    users, perhaps because they pre-configure their environments. We
    already provide a configuration file option for this but distro
    installers might have their own way of requesting this.
    
    At least the anaconda installer provides an option to skip any kind
    post-install setup tools so, for now we're only adding support for
    that but more might be added in the future.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=777708

--- daemon/gdm-display.c
+++ daemon/gdm-display.c
@@ -1548,6 +1548,31 @@ kernel_cmdline_initial_setup_force_state (gboolean *force_state)
 }
 
 static gboolean
+initial_setup_disabled_by_anaconda (void)
+{
+        GKeyFile *key_file;
+        const gchar *file_name = SYSCONFDIR "/sysconfig/anaconda";
+        gboolean disabled = FALSE;
+        GError *error = NULL;
+
+        key_file = g_key_file_new ();
+        if (!g_key_file_load_from_file (key_file, file_name, G_KEY_FILE_NONE, &error)) {
+                if (!g_error_matches (error, G_FILE_ERROR, G_FILE_ERROR_NOENT) &&
+                    !g_error_matches (error, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_NOT_FOUND)) {
+                        g_warning ("Could not read %s: %s", file_name, error->message);
+                }
+                g_error_free (error);
+                goto out;
+        }
+
+        disabled = g_key_file_get_boolean (key_file, "General",
+                                           "post_install_tools_disabled", NULL);
+ out:
+        g_key_file_unref (key_file);
+        return disabled;
+}
+
+static gboolean
 wants_initial_setup (GdmDisplay *self)
 {
         gboolean enabled = FALSE;
@@ -1584,6 +1609,10 @@ wants_initial_setup (GdmDisplay *self)
         }
 
         if (!gdm_settings_direct_get_boolean (GDM_KEY_INITIAL_SETUP_ENABLE, &enabled)) {
+                return FALSE;
+        }
+
+        if (initial_setup_disabled_by_anaconda ()) {
                 return FALSE;
         }
 
