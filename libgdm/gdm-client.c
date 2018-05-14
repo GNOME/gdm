6fce5da8e30f1a68939695a4bebe51e3d56cfc66
Ray Strode <rstrode@redhat.com>
2018-05-14 15:49:50 -0400

    libgdm: fix pointer/boolean task confusion
    The manager fetching code in GdmClient treats its task
    return value as boolean, but it's actually a pointer (the manager)
    
    This commit corrects the confusion.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=795940

--- libgdm/gdm-client.c
+++ libgdm/gdm-client.c
@@ -391,11 +391,13 @@ on_got_manager_for_reauthentication (GdmClient           *client,
                                      GTask               *task)
 {
         GCancellable *cancellable;
+        GdmManager   *manager;
         char         *username;
         GError       *error;
 
         error = NULL;
-        if (!g_task_propagate_boolean (G_TASK (result), &error)) {
+        manager = g_task_propagate_pointer (G_TASK (result), &error);
+        if (manager == NULL) {
                 g_task_return_error (task, error);
                 g_object_unref (task);
                 return;
@@ -534,10 +536,12 @@ on_got_manager_for_opening_connection (GdmClient           *client,
                                        GTask               *task)
 {
         GCancellable *cancellable;
+        GdmManager   *manager;
         GError       *error;
 
         error = NULL;
-        if (!g_task_propagate_boolean (G_TASK (result), &error)) {
+        manager = g_task_propagate_pointer (G_TASK (result), &error);
+        if (manager == NULL) {
                 g_task_return_error (task, error);
                 g_object_unref (task);
                 return;
