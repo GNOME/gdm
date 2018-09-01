GNOME/gdm

commit 80b46e2bbb53842e7c5b568dd0237fb523a021ea

manager: don't set ran_once after running initial-setup
GdmManager tracks whether or not the user session has ran
once, so it won't autologin a user again after logout.

Unfortunately the initial-setup session was counting toward the
ran_once count preventing initial-setup from logging the user
in afterward.

This commit prevents ran_once from getting set in that case.

daemon/gdm-manager.c
@@ -1484,10 +1484,12 @@ on_display_status_changed (GdmDisplay *display,
         char       *session_type = NULL;
 #ifdef WITH_PLYMOUTH
         gboolean    display_is_local = FALSE;
+        gboolean    doing_initial_setup = FALSE;
         gboolean    quit_plymouth = FALSE;
 
         g_object_get (display,
                       "is-local", &display_is_local,
+                      "doing-initial-setup", &doing_initial_setup,
                       NULL);
         quit_plymouth = display_is_local && manager->priv->plymouth_is_running;
 #endif
@@ -1528,7 +1530,7 @@ on_display_status_changed (GdmDisplay *display,
                         }
 #endif
 
-                        if (status == GDM_DISPLAY_FINISHED || g_strcmp0 (session_type, "x11") == 0) {
+                        if (!doing_initial_setup && (status == GDM_DISPLAY_FINISHED || g_strcmp0 (session_type, "x11") == 0)) {
                                 manager->priv->ran_once = TRUE;
                         }
                         maybe_start_pending_initial_login (manager, display);

commit e8f11816a30fa281a6a22ea8d635c64679cb81bd

manager: fix build with --without-plymouth
commit 80b46e2 accidentally put the
`doing_initial_setup` boolean declaration inside
a plymouth-enabled code path.

That broke the build for non-plymouth users.

This commit moves the declaration and the subsequent
initialization to unconditionalized code.

daemon/gdm-manager.c
@@ -1433,14 +1433,13 @@ on_display_status_changed (GdmDisplay *display,
         int         status;
         int         display_number = -1;
         char       *session_type = NULL;
+        gboolean    doing_initial_setup = FALSE;
 #ifdef WITH_PLYMOUTH
         gboolean    display_is_local = FALSE;
-        gboolean    doing_initial_setup = FALSE;
         gboolean    quit_plymouth = FALSE;
 
         g_object_get (display,
                       "is-local", &display_is_local,
-                      "doing-initial-setup", &doing_initial_setup,
                       NULL);
         quit_plymouth = display_is_local && manager->priv->plymouth_is_running;
 #endif
@@ -1448,6 +1447,7 @@ on_display_status_changed (GdmDisplay *display,
         g_object_get (display,
                       "x11-display-number", &display_number,
                       "session-type", &session_type,
+                      "doing-initial-setup", &doing_initial_setup,
                       NULL);
 
         status = gdm_display_get_status (display);