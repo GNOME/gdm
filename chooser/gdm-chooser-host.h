e343e9eb71c59dd65dcf4e21da4c0e4c8faa3181
Niels De Graef <nielsdegraef@gmail.com>
2018-12-23 19:28:45 +0100

    gdm-chooser-host: use G_DECLARE_FINAL_TYPE

--- chooser/gdm-chooser-host.h
+++ chooser/gdm-chooser-host.h
@@ -25,33 +25,14 @@
 
 G_BEGIN_DECLS
 
-#define GDM_TYPE_CHOOSER_HOST         (gdm_chooser_host_get_type ())
-#define GDM_CHOOSER_HOST(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), GDM_TYPE_CHOOSER_HOST, GdmChooserHost))
-#define GDM_CHOOSER_HOST_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), GDM_TYPE_CHOOSER_HOST, GdmChooserHostClass))
-#define GDM_IS_CHOOSER_HOST(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), GDM_TYPE_CHOOSER_HOST))
-#define GDM_IS_CHOOSER_HOST_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), GDM_TYPE_CHOOSER_HOST))
-#define GDM_CHOOSER_HOST_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), GDM_TYPE_CHOOSER_HOST, GdmChooserHostClass))
+#define GDM_TYPE_CHOOSER_HOST (gdm_chooser_host_get_type ())
+G_DECLARE_FINAL_TYPE (GdmChooserHost, gdm_chooser_host, GDM, CHOOSER_HOST, GObject)
 
 typedef enum {
         GDM_CHOOSER_HOST_KIND_XDMCP = 1 << 0,
 } GdmChooserHostKind;
 
 #define GDM_CHOOSER_HOST_KIND_MASK_ALL (GDM_CHOOSER_HOST_KIND_XDMCP)
-
-typedef struct GdmChooserHostPrivate GdmChooserHostPrivate;
-
-typedef struct
-{
-        GObject                parent;
-        GdmChooserHostPrivate *priv;
-} GdmChooserHost;
-
-typedef struct
-{
-        GObjectClass   parent_class;
-} GdmChooserHostClass;
-
-GType                 gdm_chooser_host_get_type            (void) G_GNUC_CONST;
 
 G_CONST_RETURN char  *gdm_chooser_host_get_description     (GdmChooserHost   *chooser_host);
 GdmAddress *          gdm_chooser_host_get_address         (GdmChooserHost   *chooser_host);
