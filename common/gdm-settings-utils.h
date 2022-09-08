GNOME/gdm

commit e1a7b7cac426f81ea2ab60263907bd92032f1068

Land the first shot at the new settings framework
svn path=/branches/mccann-gobject/; revision=4946

common/gdm-settings-utils.h
@@ -1,0 +1,53 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2007 William Jon McCann <mccann@jhu.edu>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+
+#ifndef __GDM_SETTINGS_UTILS_H
+#define __GDM_SETTINGS_UTILS_H
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+typedef struct _GdmSettingsEntry GdmSettingsEntry;
+
+GdmSettingsEntry *        gdm_settings_entry_new               (void);
+GdmSettingsEntry *        gdm_settings_entry_copy              (GdmSettingsEntry *entry);
+void                      gdm_settings_entry_free              (GdmSettingsEntry *entry);
+
+const char *              gdm_settings_entry_get_key           (GdmSettingsEntry *entry);
+const char *              gdm_settings_entry_get_signature     (GdmSettingsEntry *entry);
+const char *              gdm_settings_entry_get_default_value (GdmSettingsEntry *entry);
+
+gboolean                  gdm_settings_parse_schemas	       (const char  *file,
+								const char  *root,
+								GSList     **list);
+
+gboolean                  gdm_settings_parse_value_as_boolean  (const char *value,
+								gboolean   *bool);
+gboolean                  gdm_settings_parse_value_as_integer  (const char *value,
+								int        *intval);
+gboolean                  gdm_settings_parse_value_as_double   (const char *value,
+								gdouble    *doubleval);
+
+
+G_END_DECLS
+
+#endif /* __GDM_SETTINGS_UTILS_H */

common/Makefile.am
@@ -21,21 +21,49 @@ INCLUDES = \
 	-DGDM_DEFAULTS_CONF=\"$(GDM_DEFAULTS_CONF)\"	\
 	-DGDM_CUSTOM_CONF=\"$(GDM_CUSTOM_CONF)\"	\
 	-DGDM_OLD_CONF=\"$(GDM_OLD_CONF)\"		\
+	$(DBUS_CFLAGS)					\
 	$(GLIB_CFLAGS)
 
 noinst_LIBRARIES = 		\
 	libgdmcommon.a		\
 	$(null)
 
+BUILT_SOURCES =			\
+	gdm-settings-glue.h	\
+	gdm-marshal.c		\
+	gdm-marshal.h		\
+	$(NULL)
+
+gdm-settings-glue.h: gdm-settings.xml Makefile.am
+	dbus-binding-tool --prefix=gdm_settings --mode=glib-server --output=gdm-settings-glue.h gdm-settings.xml
+
+gdm-marshal.c: gdm-marshal.list
+	echo "#include \"gdm-marshal.h\"" > $@ && \
+	@GLIB_GENMARSHAL@ $< --prefix=gdm_marshal --body >> $@
+
+gdm-marshal.h: gdm-marshal.list
+	@GLIB_GENMARSHAL@ $< --prefix=gdm_marshal --header > $@
+
 libgdmcommon_a_SOURCES =	\
 	gdm-address.h		\
 	gdm-address.c		\
+	gdm-marshal.c		\
+	gdm-marshal.h		\
 	gdm-common.h		\
 	gdm-common.c		\
-	gdm-common-config.h	\
-	gdm-common-config.c	\
-	gdm-config.h		\
-	gdm-config.c		\
+	gdm-settings.c 		\
+	gdm-settings.h		\
+	gdm-settings-backend.c 	\
+	gdm-settings-backend.h	\
+	gdm-settings-desktop-backend.c 	\
+	gdm-settings-desktop-backend.h	\
+	gdm-settings-keys.h	\
+	gdm-settings-utils.h	\
+	gdm-settings-utils.c	\
+	gdm-settings-direct.c 	\
+	gdm-settings-direct.h	\
+	gdm-settings-client.h	\
+	gdm-settings-client.c	\
 	gdm-log.h		\
 	gdm-log.c		\
 	gdm-md5.h		\
@@ -47,18 +75,9 @@ libgdmcommon_a_SOURCES =	\
 	$(NULL)
 
 noinst_PROGRAMS = 		\
-	test-config		\
+	test-settings-server	\
+	test-settings-client	\
 	test-log		\
-	$(NULL)
-
-test_config_SOURCES = 		\
-	$(top_builddir)/daemon/gdm-daemon-config-entries.h	\
-	test-config.c	 	\
-	$(NULL)
-
-test_config_LDADD =		\
-	libgdmcommon.a	\
-	$(GLIB_LIBS)		\
 	$(NULL)
 
 test_log_SOURCES = 		\
@@ -68,4 +87,33 @@ test_log_SOURCES = 		\
 test_log_LDADD =		\
 	libgdmcommon.a	\
 	$(GLIB_LIBS)		\
+	$(NULL)
+
+test_settings_server_SOURCES = 		\
+	test-settings-server.c	 	\
+	$(NULL)
+
+test_settings_server_LDADD =		\
+	libgdmcommon.a			\
+	$(GLIB_LIBS)			\
+	$(DBUS_LIBS)			\
+	$(NULL)
+
+test_settings_client_SOURCES = 		\
+	test-settings-client.c	 	\
+	$(NULL)
+
+test_settings_client_LDADD =		\
+	libgdmcommon.a			\
+	$(GLIB_LIBS)			\
+	$(DBUS_LIBS)			\
+	$(NULL)
+
+CLEANFILES =			\
+	$(BUILT_SOURCES)	\
+	$(NULL)
+
+EXTRA_DIST = 			\
+	gdm-settings.xml	\
+	gdm-marshal.list	\
 	$(NULL)

common/gdm-address.h
@@ -38,7 +38,7 @@ G_BEGIN_DECLS
 
 typedef struct _GdmAddress GdmAddress;
 
-GType		         gdm_address_get_type	          (void);
+GType		         gdm_address_get_type	               (void);
 
 GdmAddress *             gdm_address_new_from_sockaddr_storage (struct sockaddr_storage *ss);
 

common/gdm-common-config.c
@@ -1,490 +1,0 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
- *
- * Copyright (C) 2007 William Jon McCann <mccann@jhu.edu>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
-
-#include "config.h"
-
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <locale.h>
-#include <syslog.h>
-
-#include <glib.h>
-
-#include "gdm-common-config.h"
-
-gboolean
-gdm_common_config_parse_key_string (const char *keystring,
-				    char      **group,
-				    char      **key,
-				    char      **locale,
-				    char      **value)
-{
-	char   **split1;
-	char   **split2;
-	char    *g;
-	char    *k;
-	char    *l;
-	char    *v;
-	char    *tmp1;
-	char    *tmp2;
-	gboolean ret;
-
-	g_return_val_if_fail (keystring != NULL, FALSE);
-
-	ret = FALSE;
-	g = k = v = l = NULL;
-	split1 = split2 = NULL;
-
-	g_debug ("Attempting to parse key string: %s", keystring);
-
-	split1 = g_strsplit (keystring, "/", 2);
-	if (split1 == NULL || split1 [0] == NULL || split1 [1] == NULL) {
-		goto out;
-	}
-
-	g = split1 [0];
-
-	split2 = g_strsplit (split1 [1], "=", 2);
-	if (split2 == NULL) {
-		k = split1 [1];
-	} else {
-		k = split2 [0];
-		v = split2 [1];
-	}
-
-	/* trim off the locale */
-	tmp1 = strchr (k, '[');
-	tmp2 = strchr (k, ']');
-	if (tmp1 != NULL && tmp2 != NULL && tmp2 > tmp1) {
-		l = g_strndup (tmp1 + 1, tmp2 - tmp1 - 1);
-		*tmp1 = '\0';
-	}
-
-	ret = TRUE;
- out:
-	if (group != NULL) {
-		*group = g_strdup (g);
-	}
-	if (key != NULL) {
-		*key = g_strdup (k);
-	}
-	if (locale != NULL) {
-		*locale = g_strdup (l);
-	}
-	if (value != NULL) {
-		*value = g_strdup (v);
-	}
-
-	g_strfreev (split1);
-	g_strfreev (split2);
-
-	return ret;
-}
-
-GKeyFile *
-gdm_common_config_load (const char *filename,
-			GError    **error)
-{
-	GKeyFile *config;
-	GError   *local_error;
-	gboolean  res;
-
-	config = g_key_file_new ();
-
-	local_error = NULL;
-	res = g_key_file_load_from_file (config,
-					 filename,
-					 G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS,
-					 &local_error);
-	if (! res) {
-		g_propagate_error (error, local_error);
-		g_key_file_free (config);
-		return NULL;
-	}
-
-	return config;
-}
-
-GKeyFile *
-gdm_common_config_load_from_dirs (const char  *filename,
-				  const char **dirs,
-				  GError     **error)
-{
-	GKeyFile *config;
-	int       i;
-
-	config = NULL;
-
-	/* FIXME: hope to have g_key_file_load_from_dirs
-	   see GNOME bug #355334
-	 */
-
-	for (i = 0; dirs[i] != NULL; i++) {
-		char *path;
-
-		path = g_build_filename (dirs[i], filename, NULL);
-		config = gdm_common_config_load (path, NULL);
-		g_free (path);
-		if (config != NULL) {
-			break;
-		}
-	}
-
-	if (config == NULL) {
-		g_set_error (error,
-			     G_KEY_FILE_ERROR,
-			     G_KEY_FILE_ERROR_NOT_FOUND,
-			     "Unable to find file in specified directories");
-	}
-
-	return config;
-}
-
-gboolean
-gdm_common_config_save (GKeyFile   *config,
-			const char *filename,
-			GError    **error)
-{
-	GError   *local_error;
-	gboolean  res;
-	char     *contents;
-	gsize     length;
-
-	local_error = NULL;
-	contents = g_key_file_to_data (config, &length, &local_error);
-	if (local_error != NULL) {
-		g_propagate_error (error, local_error);
-		return FALSE;
-	}
-
-	local_error = NULL;
-	res = g_file_set_contents (filename,
-				   contents,
-				   length,
-				   &local_error);
-	if (local_error != NULL) {
-		g_propagate_error (error, local_error);
-		g_free (contents);
-		return FALSE;
-	}
-
-	g_free (contents);
-	return TRUE;
-}
-
-gboolean
-gdm_common_config_get_int (GKeyFile   *config,
-			   const char *keystring,
-			   int        *value,
-			   GError    **error)
-{
-	char    *group;
-	char    *key;
-	char    *default_value;
-	int      val;
-	GError  *local_error;
-	gboolean ret;
-
-	ret = FALSE;
-
-	group = key = default_value = NULL;
-	if (! gdm_common_config_parse_key_string (keystring, &group, &key, NULL, &default_value))
-		return FALSE;
-
-	local_error = NULL;
-	val = g_key_file_get_integer (config,
-				      group,
-				      key,
-				      &local_error);
-	if (local_error != NULL) {
-		/* use the default */
-		if (default_value != NULL) {
-			ret = TRUE;
-			g_error_free (local_error);
-			val = atoi (default_value);
-		} else {
-			val = 0;
-			g_propagate_error (error, local_error);
-		}
-	} else {
-		ret = TRUE;
-	}
-
-	*value = val;
-
-	g_free (key);
-	g_free (group);
-	g_free (default_value);
-
-	return ret;
-}
-
-gboolean
-gdm_common_config_get_translated_string (GKeyFile   *config,
-					 const char *keystring,
-					 char      **value,
-					 GError    **error)
-{
-	char   *group;
-	char   *key;
-	char   *default_value;
-	char   *val;
-	const char * const *langs;
-	int     i;
-	gboolean ret;
-
-	ret = FALSE;
-
-	val = NULL;
-
-	group = key = default_value = NULL;
-	if (! gdm_common_config_parse_key_string (keystring, &group, &key, NULL, &default_value))
-		return FALSE;
-
-	langs = g_get_language_names ();
-
-	for (i = 0; langs[i] != NULL; i++) {
-		const char *locale;
-		locale = langs[i];
-
-		val = g_key_file_get_locale_string (config,
-						    group,
-						    key,
-						    locale,
-                                                    NULL);
-		if (val != NULL) {
-			break;
-		}
-	}
-
-	if (val == NULL) {
-		/* use the default */
-		if (default_value != NULL) {
-			val = g_strdup (default_value);
-			ret = TRUE;
-		}
-	} else {
-		ret = TRUE;
-	}
-
-	*value = val;
-
-	g_free (key);
-	g_free (group);
-	g_free (default_value);
-
-	return ret;
-}
-
-gboolean
-gdm_common_config_get_string (GKeyFile   *config,
-			      const char *keystring,
-			      char      **value,
-			      GError    **error)
-{
-	char    *group;
-	char    *key;
-	char    *default_value;
-	char    *val;
-	GError  *local_error;
-	gboolean ret;
-
-	ret = FALSE;
-
-	group = key = default_value = NULL;
-	if (! gdm_common_config_parse_key_string (keystring, &group, &key, NULL, &default_value)) {
-		g_set_error (error,
-			     G_KEY_FILE_ERROR,
-			     G_KEY_FILE_ERROR_PARSE,
-			     "Unable to parse key: %s",
-			     keystring);
-		return FALSE;
-	}
-
-	local_error = NULL;
-	val = g_key_file_get_string (config,
-				     group,
-				     key,
-				     &local_error);
-	if (local_error != NULL) {
-		/* use the default */
-		if (default_value != NULL) {
-			val = g_strdup (default_value);
-			ret = TRUE;
-			g_error_free (local_error);
-		} else {
-			g_propagate_error (error, local_error);
-		}
-	} else {
-		ret = TRUE;
-	}
-
-	*value = val;
-
-	g_free (key);
-	g_free (group);
-	g_free (default_value);
-
-	return ret;
-}
-
-gboolean
-gdm_common_config_get_boolean (GKeyFile   *config,
-			       const char *keystring,
-			       gboolean   *value,
-			       GError    **error)
-{
-	char    *group;
-	char    *key;
-	char    *default_value;
-	gboolean val;
-	GError  *local_error;
-	gboolean ret;
-
-	ret = FALSE;
-
-	group = key = default_value = NULL;
-	if (! gdm_common_config_parse_key_string (keystring, &group, &key, NULL, &default_value))
-		return FALSE;
-
-	local_error = NULL;
-	val = g_key_file_get_boolean (config,
-				      group,
-				      key,
-				      &local_error);
-	if (local_error != NULL) {
-		/* use the default */
-		if (default_value != NULL) {
-			if ((default_value[0] == 'T' ||
-			     default_value[0] == 't' ||
-			     default_value[0] == 'Y' ||
-			     default_value[0] == 'y' ||
-			     atoi (default_value) != 0)) {
-				val = TRUE;
-			} else {
-				val = FALSE;
-			}
-			ret = TRUE;
-			g_error_free (local_error);
-		} else {
-			g_propagate_error (error, local_error);
-		}
-	} else {
-		ret = TRUE;
-	}
-
-	*value = val;
-
-	g_free (key);
-	g_free (group);
-	g_free (default_value);
-
-	return ret;
-}
-
-void
-gdm_common_config_set_string (GKeyFile   *config,
-			      const char *keystring,
-			      const char *value)
-{
-	char    *group;
-	char    *key;
-	char    *default_value;
-
-	group = key = default_value = NULL;
-	if (! gdm_common_config_parse_key_string (keystring, &group, &key, NULL, &default_value)) {
-		return;
-	}
-
-	g_key_file_set_string (config, group, key, value);
-
-	g_free (key);
-	g_free (group);
-	g_free (default_value);
-}
-
-void
-gdm_common_config_set_boolean (GKeyFile   *config,
-			       const char *keystring,
-			       gboolean    value)
-{
-	char    *group;
-	char    *key;
-	char    *default_value;
-
-	group = key = default_value = NULL;
-	if (! gdm_common_config_parse_key_string (keystring, &group, &key, NULL, &default_value)) {
-		return;
-	}
-
-	g_key_file_set_boolean (config, group, key, value);
-
-	g_free (key);
-	g_free (group);
-	g_free (default_value);
-}
-
-void
-gdm_common_config_set_int (GKeyFile   *config,
-			   const char *keystring,
-			   int         value)
-{
-	char    *group;
-	char    *key;
-	char    *default_value;
-
-	group = key = default_value = NULL;
-	if (! gdm_common_config_parse_key_string (keystring, &group, &key, NULL, &default_value)) {
-		return;
-	}
-
-	g_key_file_set_integer (config, group, key, value);
-
-	g_free (key);
-	g_free (group);
-	g_free (default_value);
-}
-
-void
-gdm_common_config_remove_key (GKeyFile   *config,
-			      const char *keystring,
-			      GError    **error)
-{
-	char    *group;
-	char    *key;
-	char    *default_value;
-	GError  *local_error;
-
-	group = key = default_value = NULL;
-	if (! gdm_common_config_parse_key_string (keystring, &group, &key, NULL, &default_value)) {
-		return;
-	}
-
-	local_error = NULL;
-	g_key_file_remove_key (config, group, key, &local_error);
-	if (local_error != NULL) {
-		g_propagate_error (error, local_error);
-	}
-
-	g_free (key);
-	g_free (group);
-	g_free (default_value);
-}

common/gdm-common-config.h
@@ -1,75 +1,0 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
- *
- * Copyright (C) 2007 William Jon McCann <mccann@jhu.edu>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
-
-#ifndef _GDM_COMMON_CONFIG_H
-#define _GDM_COMMON_CONFIG_H
-
-#include <glib.h>
-
-G_BEGIN_DECLS
-
-GKeyFile * gdm_common_config_load             (const char *filename,
-					       GError    **error);
-GKeyFile * gdm_common_config_load_from_dirs   (const char  *filename,
-					       const char **dirs,
-					       GError    **error);
-gboolean   gdm_common_config_save             (GKeyFile   *config,
-					       const char *filename,
-					       GError    **error);
-
-gboolean   gdm_common_config_get_string       (GKeyFile   *config,
-					       const char *keystring,
-					       char      **value,
-					       GError    **error);
-gboolean   gdm_common_config_get_translated_string (GKeyFile   *config,
-						    const char *keystring,
-						    char      **value,
-						    GError    **error);
-gboolean   gdm_common_config_get_int          (GKeyFile   *config,
-					       const char *keystring,
-					       int        *value,
-					       GError    **error);
-gboolean   gdm_common_config_get_boolean      (GKeyFile   *config,
-					       const char *keystring,
-					       gboolean   *value,
-					       GError    **error);
-gboolean   gdm_common_config_parse_key_string (const char *keystring,
-					       char      **group,
-					       char      **key,
-					       char      **locale,
-					       char      **value);
-
-void       gdm_common_config_set_string       (GKeyFile   *config,
-					       const char *keystring,
-					       const char *value);
-void       gdm_common_config_set_boolean      (GKeyFile   *config,
-					       const char *keystring,
-					       gboolean    value);
-void       gdm_common_config_set_int          (GKeyFile   *config,
-					       const char *keystring,
-					       int         value);
-
-void       gdm_common_config_remove_key       (GKeyFile   *config,
-					       const char *keystring,
-					       GError    **error);
-
-G_END_DECLS
-
-#endif /* _GDM_COMMON_CONFIG_H */

common/gdm-common.h
@@ -33,8 +33,6 @@
 #include <netdb.h>
 
 #include "ve-signal.h"
-#include "gdm-common-config.h"
-#include "gdm-config.h"
 
 G_BEGIN_DECLS
 

common/gdm-config.h
@@ -1,212 +1,0 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
- *
- * Copyright (C) 2007 William Jon McCann <mccann@jhu.edu>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
-
-#ifndef _GDM_CONFIG_H
-#define _GDM_CONFIG_H
-
-#include <glib.h>
-
-G_BEGIN_DECLS
-
-typedef struct _GdmConfig GdmConfig;
-
-typedef enum {
-        GDM_CONFIG_VALUE_INVALID,
-        GDM_CONFIG_VALUE_BOOL,
-        GDM_CONFIG_VALUE_INT,
-        GDM_CONFIG_VALUE_STRING,
-        GDM_CONFIG_VALUE_LOCALE_STRING,
-        GDM_CONFIG_VALUE_STRING_ARRAY,
-        GDM_CONFIG_VALUE_LOCALE_STRING_ARRAY,
-} GdmConfigValueType;
-
-typedef enum {
-        GDM_CONFIG_SOURCE_INVALID,
-        GDM_CONFIG_SOURCE_MANDATORY,
-        GDM_CONFIG_SOURCE_DEFAULT,
-        GDM_CONFIG_SOURCE_CUSTOM,
-        GDM_CONFIG_SOURCE_BUILT_IN,
-        GDM_CONFIG_SOURCE_RUNTIME_USER,
-} GdmConfigSourceType;
-
-#define GDM_CONFIG_INVALID_ID -1
-
-struct _GdmConfigValue
-{
-	GdmConfigValueType type;
-};
-
-typedef struct _GdmConfigValue GdmConfigValue;
-
-typedef gboolean (* GdmConfigFunc) (GdmConfig          *config,
-				    GdmConfigSourceType source,
-				    const char         *group,
-				    const char         *key,
-				    GdmConfigValue     *value,
-				    int                 id,
-				    gpointer            data);
-
-typedef struct {
-	char              *group;
-	char              *key;
-	GdmConfigValueType type;
-	char              *default_value;
-	int                id;
-} GdmConfigEntry;
-
-#define GDM_CONFIG_ERROR (gdm_config_error_quark ())
-
-typedef enum
-{
-	GDM_CONFIG_ERROR_UNKNOWN_OPTION,
-	GDM_CONFIG_ERROR_BAD_VALUE,
-	GDM_CONFIG_ERROR_PARSE_ERROR,
-	GDM_CONFIG_ERROR_FAILED
-} GdmConfigError;
-
-GQuark                 gdm_config_error_quark            (void);
-
-GdmConfig *            gdm_config_new                    (void);
-void                   gdm_config_free                   (GdmConfig       *config);
-
-void                   gdm_config_set_validate_func      (GdmConfig       *config,
-							  GdmConfigFunc    func,
-							  gpointer         data);
-void                   gdm_config_set_notify_func        (GdmConfig       *config,
-							  GdmConfigFunc    func,
-							  gpointer         data);
-void                   gdm_config_set_default_file       (GdmConfig       *config,
-							  const char      *name);
-void                   gdm_config_set_mandatory_file     (GdmConfig       *config,
-							  const char      *name);
-void                   gdm_config_set_custom_file        (GdmConfig       *config,
-							  const char      *name);
-void                   gdm_config_add_entry              (GdmConfig            *config,
-							  const GdmConfigEntry *entry);
-void                   gdm_config_add_static_entries     (GdmConfig            *config,
-							  const GdmConfigEntry *entries);
-const GdmConfigEntry * gdm_config_lookup_entry           (GdmConfig            *config,
-							  const char           *group,
-							  const char           *key);
-const GdmConfigEntry * gdm_config_lookup_entry_for_id    (GdmConfig            *config,
-							  int                   id);
-
-gboolean               gdm_config_load                   (GdmConfig             *config,
-							  GError               **error);
-gboolean               gdm_config_process_all            (GdmConfig             *config,
-							  GError               **error);
-gboolean               gdm_config_process_entry          (GdmConfig             *config,
-							  const GdmConfigEntry  *entry,
-							  GError               **error);
-gboolean               gdm_config_process_entries        (GdmConfig             *config,
-							  const GdmConfigEntry **entries,
-							  gsize                  n_entries,
-							  GError               **error);
-
-gboolean               gdm_config_save_custom_file       (GdmConfig       *config,
-							  GError         **error);
-char **                gdm_config_get_keys_for_group     (GdmConfig       *config,
-							  const gchar     *group_name,
-							  gsize           *length,
-							  GError         **error);
-
-gboolean               gdm_config_peek_value             (GdmConfig             *config,
-							  const char            *group,
-							  const char            *key,
-							  const GdmConfigValue **value);
-gboolean               gdm_config_get_value              (GdmConfig       *config,
-							  const char      *group,
-							  const char      *key,
-							  GdmConfigValue **value);
-gboolean               gdm_config_set_value              (GdmConfig       *config,
-							  const char      *group,
-							  const char      *key,
-							  GdmConfigValue  *value);
-
-/* convenience functions */
-gboolean               gdm_config_get_value_for_id       (GdmConfig       *config,
-							  int              id,
-							  GdmConfigValue **value);
-gboolean               gdm_config_set_value_for_id       (GdmConfig       *config,
-							  int              id,
-							  GdmConfigValue  *value);
-
-gboolean               gdm_config_peek_string_for_id     (GdmConfig       *config,
-							  int              id,
-							  const char     **str);
-gboolean               gdm_config_get_string_for_id      (GdmConfig       *config,
-							  int              id,
-							  char           **str);
-gboolean               gdm_config_get_bool_for_id        (GdmConfig       *config,
-							  int              id,
-							  gboolean        *bool);
-gboolean               gdm_config_get_int_for_id         (GdmConfig       *config,
-							  int              id,
-							  int             *integer);
-gboolean               gdm_config_set_string_for_id      (GdmConfig       *config,
-							  int              id,
-							  char            *str);
-gboolean               gdm_config_set_bool_for_id        (GdmConfig       *config,
-							  int              id,
-							  gboolean         bool);
-gboolean               gdm_config_set_int_for_id         (GdmConfig       *config,
-							  int              id,
-							  int              integer);
-
-/* Config Values */
-
-GdmConfigValue *     gdm_config_value_new              (GdmConfigValueType    type);
-void                 gdm_config_value_free             (GdmConfigValue       *value);
-GdmConfigValue *     gdm_config_value_copy             (const GdmConfigValue *value);
-int                  gdm_config_value_compare          (const GdmConfigValue *value_a,
-							const GdmConfigValue *value_b);
-
-GdmConfigValue *     gdm_config_value_new_from_string  (GdmConfigValueType    type,
-							const char           *str,
-							GError              **error);
-const char *         gdm_config_value_get_string       (const GdmConfigValue *value);
-const char *         gdm_config_value_get_locale_string       (const GdmConfigValue *value);
-const char **        gdm_config_value_get_string_array (const GdmConfigValue *value);
-
-int                  gdm_config_value_get_int          (const GdmConfigValue *value);
-gboolean             gdm_config_value_get_bool         (const GdmConfigValue *value);
-
-void                 gdm_config_value_set_string       (GdmConfigValue  *value,
-							const char      *str);
-void                 gdm_config_value_set_locale_string       (GdmConfigValue  *value,
-							       const char      *str);
-
-void                 gdm_config_value_set_string_array (GdmConfigValue  *value,
-							const char     **array);
-void                 gdm_config_value_set_locale_string_array (GdmConfigValue  *value,
-							       const char     **array);
-void                 gdm_config_value_set_int          (GdmConfigValue  *value,
-							int              integer);
-void                 gdm_config_value_set_bool         (GdmConfigValue  *value,
-							gboolean         bool);
-char *               gdm_config_value_to_string        (const GdmConfigValue *value);
-
-/* Config Entries */
-GdmConfigEntry *     gdm_config_entry_copy             (const GdmConfigEntry *entry);
-void                 gdm_config_entry_free             (GdmConfigEntry       *entry);
-
-G_END_DECLS
-
-#endif /* _GDM_CONFIG_H */

commit 43a9c9e0a7d9235914885ce50c0b213728ab52df

gdm-settings-utils: Remove leftovers
This function doesn't exist.

Fixes: e1a7b7cac426f81ea2ab60263907bd92032f1068

common/gdm-settings-utils.h
@@ -29,7 +29,6 @@ G_BEGIN_DECLS
 typedef struct _GdmSettingsEntry GdmSettingsEntry;
 
 GdmSettingsEntry *        gdm_settings_entry_new               (void);
-GdmSettingsEntry *        gdm_settings_entry_copy              (GdmSettingsEntry *entry);
 void                      gdm_settings_entry_free              (GdmSettingsEntry *entry);
 
 const char *              gdm_settings_entry_get_key           (GdmSettingsEntry *entry);