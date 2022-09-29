3a8e7d2eb063f9604c11d52a4c0bed67eb5d8205
Alessandro Bono <alessandro.bono369@gmail.com>
2022-09-29 11:26:45 +0200

    gdm-display: Simplify code
    Just pass the error we received from the caller. While this doesn't
    change much, it avoids a scan-build warning:
    
    ../daemon/gdm-display.c:213:73: warning: Access to field 'message' results in a dereference of a null pointer (loaded from variable 'error') [core.NullDereference]
                    g_critical ("could not create display access file: %s", error->message);
                                                                            ^~~~~~~~~~~~~~

--- daemon/gdm-display.c
+++ daemon/gdm-display.c
@@ -178,13 +178,9 @@ _create_access_file_for_user (GdmDisplay  *self,
                               GError     **error)
 {
         GdmDisplayAccessFile *access_file;
-        GError *file_error;
 
         access_file = gdm_display_access_file_new (username);
-
-        file_error = NULL;
-        if (!gdm_display_access_file_open (access_file, &file_error)) {
-                g_propagate_error (error, file_error);
+        if (!gdm_display_access_file_open (access_file, error)) {
                 return NULL;
         }
 
