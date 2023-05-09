GNOME/gdm

commit f0252728e9ef2a23b0d98a4fe2c697fdc673d254

gdm-session-settings: Remove unused variable

daemon/gdm-session-settings.c
@@ -338,26 +338,18 @@ gboolean
 gdm_session_settings_load (GdmSessionSettings  *settings,
                            const char          *username)
 {
-        ActUser *old_user;
-
         g_return_val_if_fail (GDM_IS_SESSION_SETTINGS (settings), FALSE);
         g_return_val_if_fail (username != NULL, FALSE);
         g_return_val_if_fail (!gdm_session_settings_is_loaded (settings), FALSE);
 
         if (settings->user != NULL) {
-                old_user = settings->user;
-
                 g_signal_handlers_disconnect_by_func (G_OBJECT (settings->user),
                                                       G_CALLBACK (on_user_is_loaded_changed),
                                                       settings);
-        } else {
-                old_user = NULL;
         }
 
         settings->user = act_user_manager_get_user (settings->user_manager,
                                                           username);
-
-        g_clear_object (&old_user);
 
         if (!act_user_is_loaded (settings->user)) {
                 g_signal_connect (settings->user,

commit 95736e372a11d00f02054906e8286b6f35cf5cd3

gdm-session-settings: Plug a leak
Fallout from f0252728e9ef2a23b0d98a4fe2c697fdc673d254. The variable
was used to free the object pointed by settings->user after
replacing it.

daemon/gdm-session-settings.c
@@ -338,12 +338,15 @@ gboolean
 gdm_session_settings_load (GdmSessionSettings  *settings,
                            const char          *username)
 {
+        g_autoptr(ActUser) old_user = NULL;
+
         g_return_val_if_fail (GDM_IS_SESSION_SETTINGS (settings), FALSE);
         g_return_val_if_fail (username != NULL, FALSE);
         g_return_val_if_fail (!gdm_session_settings_is_loaded (settings), FALSE);
 
-        if (settings->user != NULL) {
-                g_signal_handlers_disconnect_by_func (G_OBJECT (settings->user),
+        old_user = g_steal_pointer (&settings->user);
+        if (old_user != NULL) {
+                g_signal_handlers_disconnect_by_func (G_OBJECT (old_user),
                                                       G_CALLBACK (on_user_is_loaded_changed),
                                                       settings);
         }