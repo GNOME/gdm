e75ab4ac280d7f126f13f693267c5eac65984fbe
Adrian Vovk <adrianvovk@gmail.com>
2025-11-12 21:53:39 -0500

    display: Drop gdm_display_manage
    This no longer does anything other than calling into gdm_display_prepare

--- daemon/gdm-display.c
+++ daemon/gdm-display.c
@@ -328,35 +328,6 @@ gdm_display_prepare (GdmDisplay *self)
 }
 
 gboolean
-gdm_display_manage (GdmDisplay *self)
-{
-        GdmDisplayPrivate *priv;
-        gboolean res;
-
-        g_return_val_if_fail (GDM_IS_DISPLAY (self), FALSE);
-
-        priv = gdm_display_get_instance_private (self);
-
-        g_debug ("GdmDisplay: Managing display: %s", priv->id);
-
-        /* If not explicitly prepared, do it now */
-        if (priv->status == GDM_DISPLAY_UNMANAGED) {
-                res = gdm_display_prepare (self);
-                if (! res) {
-                        return FALSE;
-                }
-        }
-
-        if (g_strcmp0 (priv->session_class, "greeter") == 0) {
-                if (GDM_DISPLAY_GET_CLASS (self)->manage != NULL) {
-                        GDM_DISPLAY_GET_CLASS (self)->manage (self);
-                }
-        }
-
-        return TRUE;
-}
-
-gboolean
 gdm_display_finish (GdmDisplay *self)
 {
         GdmDisplayPrivate *priv;
