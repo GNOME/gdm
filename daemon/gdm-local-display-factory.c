GNOME/gdm

commit deeb4b8aba46e37a1f6dcb85252ed713183cb170

manager: Combine register display with register session
Before we'd always register the display as soon as the session was
started. However, this is too early for Wayland! The compositor might
not have completely initialized its Wayland connection yet, so the
display isn't really open.

The "register display" should happen when the compositor is up and
running. This occurs when it calls RegisterSession, so it makes sense
to combine both.

This helps on terminating plymouthd when the new session has been
started, and also kill background greeters.

Part-of: <https://gitlab.gnome.org/GNOME/gdm/-/merge_requests/285>

daemon/gdm-local-display-factory.c
@@ -513,25 +513,6 @@ on_finish_waiting_for_seat0_displays_timeout (GdmLocalDisplayFactory *factory)
 }
 
 static void
-on_session_registered_cb (GObject *gobject,
-                          GParamSpec *pspec,
-                          gpointer user_data)
-{
-        GdmDisplay *display = GDM_DISPLAY (gobject);
-        GdmLocalDisplayFactory *factory = GDM_LOCAL_DISPLAY_FACTORY (user_data);
-        gboolean registered;
-
-        g_object_get (display, "session-registered", &registered, NULL);
-
-        if (!registered)
-                return;
-
-        g_debug ("GdmLocalDisplayFactory: session registered on display, looking for any background displays to kill");
-
-        finish_waiting_displays_on_seat (factory, "seat0");
-}
-
-static void
 on_display_status_changed (GdmDisplay             *display,
                            GParamSpec             *arg1,
                            GdmLocalDisplayFactory *factory)
@@ -545,6 +526,7 @@ on_display_status_changed (GdmDisplay             *display,
         char            *session_id = NULL;
         gboolean         is_initial = TRUE;
         gboolean         is_local = TRUE;
+        gboolean         registered = FALSE;
 
 
         if (!factory->is_started)
@@ -611,11 +593,11 @@ on_display_status_changed (GdmDisplay             *display,
         case GDM_DISPLAY_PREPARED:
                 break;
         case GDM_DISPLAY_MANAGED:
-                g_signal_connect_object (display,
-                                         "notify::session-registered",
-                                         G_CALLBACK (on_session_registered_cb),
-                                         factory,
-                                         0);
+                g_object_get (display, "session-registered", &registered, NULL);
+                if (registered) {
+                        g_debug ("GdmLocalDisplayFactory: session registered on display, looking for any background displays to kill");
+                        finish_waiting_displays_on_seat (factory, "seat0");
+                }
                 break;
         case GDM_DISPLAY_WAITING_TO_FINISH:
                 break;

daemon/gdm-manager.c
@@ -730,7 +730,7 @@ find_user_session_for_display (GdmManager *self,
 }
 
 static gboolean
-gdm_manager_handle_register_display (GdmDBusManager        *manager,
+gdm_manager_handle_register_session (GdmDBusManager        *manager,
                                      GDBusMethodInvocation *invocation,
                                      GVariant              *details)
 {
@@ -742,14 +742,14 @@ gdm_manager_handle_register_display (GdmDBusManager        *manager,
         GVariantIter     iter;
         char            *key = NULL;
         char            *value = NULL;
-        char            *x11_display_name = NULL;
-        char            *tty = NULL;
-
-        g_debug ("GdmManager: trying to register new display");
+        g_autofree char *x11_display_name = NULL;
+        g_autofree char *tty = NULL;
 
         sender = g_dbus_method_invocation_get_sender (invocation);
         connection = g_dbus_method_invocation_get_connection (invocation);
         get_display_and_details_for_bus_sender (self, connection, sender, &display, NULL, NULL, &tty, NULL, NULL, NULL, NULL);
+
+        g_debug ("GdmManager: trying to register new session on display %p", display);
 
         if (display == NULL) {
                 g_dbus_method_invocation_return_error_literal (invocation,
@@ -790,38 +790,10 @@ gdm_manager_handle_register_display (GdmDBusManager        *manager,
                 }
         }
 
-        g_object_set (G_OBJECT (display), "status", GDM_DISPLAY_MANAGED, NULL);
-
-        gdm_dbus_manager_complete_register_display (GDM_DBUS_MANAGER (manager),
-                                                    invocation);
-
-        g_clear_pointer (&x11_display_name, g_free);
-        g_clear_pointer (&tty, g_free);
-        return TRUE;
-}
-
-static gboolean
-gdm_manager_handle_register_session (GdmDBusManager        *manager,
-                                     GDBusMethodInvocation *invocation,
-                                     GVariant              *details)
-{
-        GdmManager      *self = GDM_MANAGER (manager);
-        GdmDisplay      *display = NULL;
-        const char      *sender;
-        GDBusConnection *connection;
-
-        sender = g_dbus_method_invocation_get_sender (invocation);
-        connection = g_dbus_method_invocation_get_connection (invocation);
-
-        get_display_and_details_for_bus_sender (self, connection, sender, &display,
-                                                NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
-        g_debug ("GdmManager: trying to register new session on display %p", display);
-
-        if (display != NULL)
-                g_object_set (G_OBJECT (display), "session-registered", TRUE, NULL);
-        else
-                g_debug ("GdmManager: No display, not registering");
+        g_object_set (G_OBJECT (display),
+                      "status", GDM_DISPLAY_MANAGED,
+                      "session-registered", TRUE,
+                      NULL);
 
         gdm_dbus_manager_complete_register_session (GDM_DBUS_MANAGER (manager),
                                                     invocation);
@@ -1222,7 +1194,6 @@ gdm_manager_handle_open_reauthentication_channel (GdmDBusManager        *manager
 static void
 manager_interface_init (GdmDBusManagerIface *interface)
 {
-        interface->handle_register_display = gdm_manager_handle_register_display;
         interface->handle_register_session = gdm_manager_handle_register_session;
         interface->handle_open_session = gdm_manager_handle_open_session;
         interface->handle_open_reauthentication_channel = gdm_manager_handle_open_reauthentication_channel;

daemon/gdm-manager.xml
@@ -1,9 +1,6 @@
 <!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN" "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
 <node name="/org/gnome/DisplayManager/Manager">
   <interface name="org.gnome.DisplayManager.Manager">
-    <method name="RegisterDisplay">
-      <arg name="details" direction="in" type="a{ss}"/>
-    </method>
     <method name="RegisterSession">
       <arg name="details" direction="in" type="a{sv}"/>
     </method>

daemon/gdm-wayland-session.c
@@ -404,29 +404,6 @@ wait_on_subprocesses (State *state)
         }
 }
 
-static gboolean
-register_display (State        *state,
-                  GCancellable *cancellable)
-{
-        GError          *error = NULL;
-        gboolean         registered = FALSE;
-        GVariantBuilder  details;
-
-        g_variant_builder_init (&details, G_VARIANT_TYPE ("a{ss}"));
-        g_variant_builder_add (&details, "{ss}", "session-type", "wayland");
-
-        registered = gdm_dbus_manager_call_register_display_sync (state->display_manager_proxy,
-                                                                  g_variant_builder_end (&details),
-                                                                  cancellable,
-                                                                  &error);
-        if (error != NULL) {
-                g_debug ("Could not register display: %s", error->message);
-                g_error_free (error);
-        }
-
-        return registered;
-}
-
 static void
 init_state (State **state)
 {
@@ -583,14 +560,6 @@ main (int    argc,
 
         if (!connect_to_display_manager (state))
                 goto out;
-
-        ret = register_display (state, state->cancellable);
-
-        if (!ret) {
-                g_printerr ("Unable to register display with display manager\n");
-                exit_status = EX_SOFTWARE;
-                goto out;
-        }
 
         if (register_session) {
                 g_debug ("gdm-wayland-session: Will register session in %d seconds", REGISTER_SESSION_TIMEOUT);

daemon/gdm-x-session.c
@@ -760,30 +760,6 @@ wait_on_subprocesses (State *state)
         }
 }
 
-static gboolean
-register_display (State        *state,
-                  GCancellable *cancellable)
-{
-        GError          *error = NULL;
-        gboolean         registered = FALSE;
-        GVariantBuilder  details;
-
-        g_variant_builder_init (&details, G_VARIANT_TYPE ("a{ss}"));
-        g_variant_builder_add (&details, "{ss}", "session-type", "x11");
-        g_variant_builder_add (&details, "{ss}", "x11-display-name", state->display_name);
-
-        registered = gdm_dbus_manager_call_register_display_sync (state->display_manager_proxy,
-                                                                  g_variant_builder_end (&details),
-                                                                  cancellable,
-                                                                  &error);
-        if (error != NULL) {
-                g_debug ("Could not register display: %s", error->message);
-                g_error_free (error);
-        }
-
-        return registered;
-}
-
 static void
 init_state (State **state)
 {
@@ -955,14 +931,6 @@ main (int    argc,
 
         if (!connect_to_display_manager (state))
                 goto out;
-
-        ret = register_display (state, state->cancellable);
-
-        if (!ret) {
-                g_printerr ("Unable to register display with display manager\n");
-                exit_status = EX_SOFTWARE;
-                goto out;
-        }
 
         ret = spawn_session (state, run_script, state->cancellable);
 

commit ecc9ab663cd698faa2eef596789f43df50976534

manager: Add RegisterDisplay again
1. RegisterSession is used to record a session login. It should be
   called when the new session is started.
2. RegisterDisplay is used to terminate pending greeters and plymouth
   to reach graphical.target. It should be called once the new session
   completes its graphics startup.

RegisterSession was trying to do both before new session startup was completed.
This had a potential issue: the new session is setting up while the greeter
session is tearing down leading to configuration conflicts.

Fixes: deeb4b8aba46e37a1f6dcb85252ed713183cb170 ("manager: Combine register
display with register session")

daemon/gdm-local-display-factory.c
@@ -264,8 +264,6 @@ on_display_status_changed (GdmDisplay             *display,
         char            *session_id = NULL;
         gboolean         is_initial = TRUE;
         gboolean         is_local = TRUE;
-        gboolean         registered = FALSE;
-
 
         if (!factory->is_started)
                 return;
@@ -320,11 +318,7 @@ on_display_status_changed (GdmDisplay             *display,
         case GDM_DISPLAY_PREPARED:
                 break;
         case GDM_DISPLAY_MANAGED:
-                g_object_get (display, "session-registered", &registered, NULL);
-                if (registered) {
-                        g_debug ("GdmLocalDisplayFactory: session registered on display, looking for any background displays to kill");
-                        finish_waiting_displays_on_seat (factory, "seat0");
-                }
+                finish_waiting_displays_on_seat (factory, "seat0");
                 break;
         case GDM_DISPLAY_WAITING_TO_FINISH:
                 break;

daemon/gdm-manager.c
@@ -649,6 +649,38 @@ find_user_session_for_display (GdmManager *self,
 }
 
 static gboolean
+gdm_manager_handle_register_display (GdmDBusManager        *manager,
+                                     GDBusMethodInvocation *invocation)
+{
+        GdmManager      *self = GDM_MANAGER (manager);
+        const char      *sender;
+        GDBusConnection *connection;
+        GdmDisplay      *display = NULL;
+
+        sender = g_dbus_method_invocation_get_sender (invocation);
+        connection = g_dbus_method_invocation_get_connection (invocation);
+        get_display_and_details_for_bus_sender (self, connection, sender, &display, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+        if (display == NULL) {
+                g_dbus_method_invocation_return_error_literal (invocation,
+                                                               G_DBUS_ERROR,
+                                                               G_DBUS_ERROR_ACCESS_DENIED,
+                                                               _("No display available"));
+
+                return G_DBUS_METHOD_INVOCATION_HANDLED;
+        }
+
+        g_object_set (G_OBJECT (display),
+                      "status", GDM_DISPLAY_MANAGED,
+                      NULL);
+
+        gdm_dbus_manager_complete_register_display (GDM_DBUS_MANAGER (manager),
+                                                    invocation);
+
+        return G_DBUS_METHOD_INVOCATION_HANDLED;
+}
+
+static gboolean
 gdm_manager_handle_register_session (GdmDBusManager        *manager,
                                      GDBusMethodInvocation *invocation)
 {
@@ -671,7 +703,7 @@ gdm_manager_handle_register_session (GdmDBusManager        *manager,
                                                                G_DBUS_ERROR_ACCESS_DENIED,
                                                                _("No display available"));
 
-                return TRUE;
+                return G_DBUS_METHOD_INVOCATION_HANDLED;
         }
 
         session = find_user_session_for_display (self, display);
@@ -686,14 +718,13 @@ gdm_manager_handle_register_session (GdmDBusManager        *manager,
         }
 
         g_object_set (G_OBJECT (display),
-                      "status", GDM_DISPLAY_MANAGED,
                       "session-registered", TRUE,
                       NULL);
 
         gdm_dbus_manager_complete_register_session (GDM_DBUS_MANAGER (manager),
                                                     invocation);
 
-        return TRUE;
+        return G_DBUS_METHOD_INVOCATION_HANDLED;
 }
 
 static gboolean
@@ -1084,6 +1115,7 @@ gdm_manager_handle_open_reauthentication_channel (GdmDBusManager        *manager
 static void
 manager_interface_init (GdmDBusManagerIface *interface)
 {
+        interface->handle_register_display = gdm_manager_handle_register_display;
         interface->handle_register_session = gdm_manager_handle_register_session;
         interface->handle_open_session = gdm_manager_handle_open_session;
         interface->handle_open_reauthentication_channel = gdm_manager_handle_open_reauthentication_channel;

daemon/gdm-manager.xml
@@ -1,6 +1,8 @@
 <!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN" "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
 <node name="/org/gnome/DisplayManager/Manager">
   <interface name="org.gnome.DisplayManager.Manager">
+    <method name="RegisterDisplay">
+    </method>
     <method name="RegisterSession">
     </method>
     <method name="OpenSession">