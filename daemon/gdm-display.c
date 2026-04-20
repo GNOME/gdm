4a132e2d8bec1f83207ce02652a6d1b3a3657e2c
Bilal Elmoussaoui <belmouss@redhat.com>
2026-04-20 22:32:42 +0200

    Prefer g_object_notify_by_pspec
    Avoids the pspec by name lookup

--- daemon/gdm-display.c
+++ daemon/gdm-display.c
@@ -240,7 +240,7 @@ _gdm_display_set_status (GdmDisplay *self,
         priv = gdm_display_get_instance_private (self);
         if (status != priv->status) {
                 priv->status = status;
-                g_object_notify (G_OBJECT (self), "status");
+                g_object_notify_by_pspec (G_OBJECT (self), props[PROP_STATUS]);
         }
 }
 
