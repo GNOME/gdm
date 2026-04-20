da974c3e50381bb0a5c1c621f78c44c0776e7221
Bilal Elmoussaoui <belmouss@redhat.com>
2026-04-20 20:50:17 +0200

    Prefer using g_set_object

--- daemon/gdm-display.c
+++ daemon/gdm-display.c
@@ -491,9 +491,8 @@ _gdm_display_set_launch_environment (GdmDisplay           *self,
 
         priv = gdm_display_get_instance_private (self);
 
-        g_clear_object (&priv->launch_environment);
 
-        priv->launch_environment = g_object_ref (launch_environment);
+        g_set_object (&priv->launch_environment, launch_environment);
 }
 
 static void
