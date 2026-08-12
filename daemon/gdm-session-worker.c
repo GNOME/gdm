TheDegenerateDev5150/gdm-MASTER

commit e760c8e4950dfd3de7a1062be0d2c2dc18e70c73

session-worker: Explicitly check and load gdm services
Load service files from both $SYSCONFIGDIR/pam.d and $SYSLIB/pam.d,
giving the configuration path preference.

Also load the service file explicitly.
This ensures that we are opening the service file we expect, but without
blocking it to access to other included service files or modules as
pam_start_confdir may do.

In case a service file is not found, we just rely on the config files
path.

Part-of: <https://gitlab.gnome.org/GNOME/gdm/-/merge_requests/380>

daemon/gdm-session-worker.c
@@ -1168,6 +1168,24 @@ gdm_session_worker_uninitialize_pam (GdmSessionWorker *worker,
         gdm_session_worker_set_state (worker, GDM_SESSION_WORKER_STATE_NONE);
 }
 
+static gchar *
+gdm_session_worker_resolve_service_file (const char *service)
+{
+        g_autofree char *service_path = NULL;
+
+        g_set_str (&service_path, g_build_filename (PAM_CONFIG_SERVICES_DIR, service, NULL));
+        if (g_file_test (service_path, G_FILE_TEST_EXISTS|G_FILE_TEST_IS_REGULAR)) {
+                return g_steal_pointer (&service_path);
+        }
+
+        g_set_str (&service_path, g_build_filename (PAM_SYS_SERVICES_DIR, service, NULL));
+        if (g_file_test (service_path, G_FILE_TEST_EXISTS|G_FILE_TEST_IS_REGULAR)) {
+                return g_steal_pointer (&service_path);
+        }
+
+        return NULL;
+}
+
 static gboolean
 gdm_session_worker_initialize_pam (GdmSessionWorker   *worker,
                                    const char         *service,
@@ -1182,6 +1200,7 @@ gdm_session_worker_initialize_pam (GdmSessionWorker   *worker,
         struct pam_conv        pam_conversation;
         int                    error_code;
         char tty_string[256];
+        g_autofree char *service_path = NULL;
 
         g_assert (service != NULL);
         g_assert (worker->pam_handle == NULL);
@@ -1190,6 +1209,25 @@ gdm_session_worker_initialize_pam (GdmSessionWorker   *worker,
                  service ? service : "(null)",
                  username ? username : "(null)",
                  seat_id ? seat_id : "(null)");
+
+        service_path = gdm_session_worker_resolve_service_file (service);
+        if (!service_path) {
+                g_debug ("GdmSessionWorker: PAM profile %s was not found", service);
+                g_set_error (error,
+                             GDM_SESSION_WORKER_ERROR,
+                             GDM_SESSION_WORKER_ERROR_SERVICE_UNAVAILABLE,
+                             "PAM profile %s was not found", service);
+
+                /* There is not a specific error for service not found, so we just
+                 * error out as the module is missing, although this is not really
+                 * used since no pam handle is set, thus it's just used to keep
+                 * the same "out" logic.
+                 */
+                error_code = PAM_MODULE_UNKNOWN;
+                goto out;
+        }
+
+        g_debug ("GdmSessionWorker: using PAM profile %s", service_path);
 
 #ifdef SUPPORTS_PAM_EXTENSIONS
         if (extensions != NULL) {
@@ -1201,7 +1239,7 @@ gdm_session_worker_initialize_pam (GdmSessionWorker   *worker,
         pam_conversation.appdata_ptr = worker;
 
         gdm_session_worker_start_auditor (worker);
-        error_code = pam_start (service,
+        error_code = pam_start (service_path,
                                 username,
                                 &pam_conversation,
                                 &worker->pam_handle);

GNOME/gdm

commit fcd91e112cd979dc19eec06b87eae9734ced9984

session-worker: Don't pass path into pam_start
A previous commit added code to check for the presence of a PAM service
when initializing the PAM conversation. However, it also passed the path
to the found PAM service into pam_start

This is incorrect. PAM does not allow file paths passed into pam_start.
In practice, rather than throwing an error PAM just used the basename of
the supplied path. This masked the mistake.

This fixes the code to pass a service name into PAM. We still should
check for the presence of the PAM service before trying to activate it
via PAM, though.

Fixes: e760c8e4950dfd3de7a1062be0d2c2dc18e70c73

daemon/gdm-session-worker.c
@@ -1259,7 +1259,8 @@ gdm_session_worker_initialize_pam (GdmSessionWorker   *worker,
                 goto out;
         }
 
-        g_debug ("GdmSessionWorker: using PAM profile %s", service_path);
+        g_debug ("GdmSessionWorker: found PAM profile %s at %s. Continuing",
+                 service, service_path);
 
 #ifdef SUPPORTS_PAM_EXTENSIONS
         if (extensions != NULL) {
@@ -1273,7 +1274,7 @@ gdm_session_worker_initialize_pam (GdmSessionWorker   *worker,
         pam_conversation.appdata_ptr = worker;
 
         gdm_session_worker_start_auditor (worker);
-        error_code = pam_start (service_path,
+        error_code = pam_start (service,
                                 username,
                                 &pam_conversation,
                                 &worker->pam_handle);