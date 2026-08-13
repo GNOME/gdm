GNOME/gdm

commit 4ecd58d766840835598e5b6c63088460dba6ac4d

pam_gdm: Bound the cached passphrase walk
The cached passphrase blob read from the kernel keyring is a
NUL-separated list of passwords, but keyctl_read_alloc() returns an
arbitrary byte buffer that is not guaranteed to be NUL-terminated.
Walking it with strlen() could therefore read past the end of the
allocation. Reject a payload whose final entry is not terminated
within the buffer and bound each step of the walk with strnlen()
using the number of remaining bytes.

Part-of: <https://gitlab.gnome.org/GNOME/gdm/-/merge_requests/384>

pam_gdm/pam_gdm.c
@@ -19,6 +19,7 @@
  */
 #include <config.h>
 
+#include <string.h>
 #include <unistd.h>
 
 #include <security/_pam_macros.h>
@@ -52,8 +53,20 @@ pam_sm_authenticate (pam_handle_t  *pamh,
         r = keyctl_read_alloc (serial, &cached_passwords);
         if (r < 0)
                 return PAM_AUTHINFO_UNAVAIL;
-        
+
         cached_passwords_length = r;
+
+        /*
+            The payload is a NUL-separated list of passwords, but the blob
+            returned by the kernel is not guaranteed to be NUL-terminated.
+            Reject a payload whose final entry is not terminated within the
+            buffer so that walking it below cannot read past the end.
+        */
+        if (cached_passwords_length == 0 ||
+            ((char *) cached_passwords)[cached_passwords_length - 1] != '\0') {
+                free (cached_passwords);
+                return PAM_AUTHINFO_UNAVAIL;
+        }
 
         /*
             Find the last password in the NUL-separated list of passwords.
@@ -66,8 +79,9 @@ pam_sm_authenticate (pam_handle_t  *pamh,
         */
         last_cached_password = cached_passwords;
         for (i = 0; i < cached_passwords_length; i++) {
+                size_t remaining = cached_passwords_length - i;
                 last_cached_password = ((char *) cached_passwords) + i;
-                i += strlen (last_cached_password);
+                i += strnlen (last_cached_password, remaining);
         }
 
         r = pam_set_item (pamh, PAM_AUTHTOK, last_cached_password);

commit ca0050125a064eb291edbfe77abb2f2688d30501

pam_gdm: Don't reject unterminated buffers
The data returned by the kernel keyring is a list of strings separated
by their null terminators. However, the might not be a terminator on the
very end

Previously we were using strlen(), which meant we could read past the
end of the array. A previous security patch was a little overzealous,
and rather than just fixing the strlen() call it _also_ completely
rejected unterminated buffers.

However, systemd _intentionally_ does not terminate the buffer when it
creates it. This means that, after this security patch, pam_gdm
basically stopped working. Let's fix the regression but keep the
underlying bugfix

Fixes: 4ecd58d766840835598e5b6c63088460dba6ac4d

Closes: https://gitlab.gnome.org/GNOME/gdm/-/work_items/1090

pam_gdm/pam_gdm.c
@@ -67,20 +67,6 @@ pam_sm_authenticate (pam_handle_t  *pamh,
                 return PAM_AUTHINFO_UNAVAIL;
 
         cached_passwords_length = r;
-
-        /*
-            The payload is a NUL-separated list of passwords, but the blob
-            returned by the kernel is not guaranteed to be NUL-terminated.
-            Reject a payload whose final entry is not terminated within the
-            buffer so that walking it below cannot read past the end.
-        */
-        if (cached_passwords_length == 0 ||
-            ((char *) cached_passwords)[cached_passwords_length - 1] != '\0') {
-                gdm_pam_zero_buffer (cached_passwords, cached_passwords_length);
-                free (cached_passwords);
-                return PAM_AUTHINFO_UNAVAIL;
-        }
-
         /*
             Find the last password in the NUL-separated list of passwords.
             Multiple passwords are returned either when the user enters an