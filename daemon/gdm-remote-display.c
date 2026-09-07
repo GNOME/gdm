c2a25ce2e7320fcc45fb2b6073e3c55b86406d00
Joan Torres Lopez <joantolo@redhat.com>
2026-09-07 20:44:02 +0200

    remote-display: Defer D-Bus export until session-id is known
    Clients shouldn't need to watch "notify::session-id" before using a
    RemoteDisplay, since they can't guarantee they'll see the change after
    the object appears on the bus.
    
    Override GdmDisplay's "export" vmethod to wait for a session-id before
    exporting, with a 3 second timeout as a safety net.

--- daemon/gdm-remote-display.c
+++ daemon/gdm-remote-display.c
@@ -27,17 +27,105 @@
 #include "gdm-remote-display.h"
 #include "gdm-remote-display-glue.h"
 
+#define REMOTE_DISPLAY_EXPORT_TIMEOUT_SECONDS 3
+
+typedef struct
+{
+        GdmRemoteDisplay         *display;
+        GDBusObjectManagerServer *object_manager;
+        guint                     timeout_id;
+} PendingExport;
+
 struct _GdmRemoteDisplay
 {
         GdmDisplay            parent;
 
         GdmDBusRemoteDisplay *skeleton;
+        PendingExport        *pending_export;
 };
 
 static void     gdm_remote_display_class_init   (GdmRemoteDisplayClass *klass);
 static void     gdm_remote_display_init         (GdmRemoteDisplay      *remote_display);
+static void     on_pending_export_session_id_set (GdmDisplay *display,
+                                                  GParamSpec *pspec,
+                                                  gpointer    user_data);
 
 G_DEFINE_TYPE (GdmRemoteDisplay, gdm_remote_display, GDM_TYPE_DISPLAY)
+
+static void
+pending_export_free (PendingExport *pending_export)
+{
+        g_signal_handlers_disconnect_by_func (pending_export->display,
+                                              on_pending_export_session_id_set,
+                                              NULL);
+
+        g_clear_handle_id (&pending_export->timeout_id, g_source_remove);
+        g_clear_object (&pending_export->object_manager);
+        g_free (pending_export);
+}
+
+static void
+finish_pending_export (GdmRemoteDisplay *self)
+{
+        GdmDisplay *display = GDM_DISPLAY (self);
+        PendingExport *pending_export = g_steal_pointer (&self->pending_export);
+        GDBusObjectManagerServer *object_manager = pending_export->object_manager;
+
+        GDM_DISPLAY_CLASS (gdm_remote_display_parent_class)->export (display, object_manager);
+
+        pending_export_free (pending_export);
+}
+
+static void
+on_pending_export_session_id_set (GdmDisplay *display,
+                                  GParamSpec *pspec,
+                                  gpointer    user_data)
+{
+        if (gdm_display_get_session_id (display) == NULL)
+                return;
+
+        finish_pending_export (GDM_REMOTE_DISPLAY (display));
+}
+
+static void
+on_pending_export_timeout (gpointer user_data)
+{
+        GdmRemoteDisplay *self = GDM_REMOTE_DISPLAY (user_data);
+        g_autofree char *id;
+
+        gdm_display_get_id (GDM_DISPLAY (self), &id, NULL);
+
+        g_debug ("Timeout waiting for session-id to be set on remote display %p, "
+                 "exporting anyway", id);
+
+        finish_pending_export (self);
+}
+
+static void
+gdm_remote_display_export (GdmDisplay               *display,
+                           GDBusObjectManagerServer *object_manager)
+{
+        GdmRemoteDisplay *self = GDM_REMOTE_DISPLAY (display);
+        PendingExport *pending_export;
+
+        if (gdm_display_get_session_id (display) != NULL) {
+                GDM_DISPLAY_CLASS (gdm_remote_display_parent_class)->export (display, object_manager);
+                return;
+        }
+
+        pending_export = g_new0 (PendingExport, 1);
+        pending_export->display = self;
+        pending_export->object_manager = g_object_ref (object_manager);
+        g_signal_connect (self, "notify::session-id",
+                          G_CALLBACK (on_pending_export_session_id_set),
+                          NULL);
+        pending_export->timeout_id =
+                g_timeout_add_seconds_once (REMOTE_DISPLAY_EXPORT_TIMEOUT_SECONDS,
+                                            on_pending_export_timeout,
+                                            self);
+
+        self->pending_export = pending_export;
+}
 
 char *
 gdm_remote_display_get_remote_id (GdmRemoteDisplay *display)
@@ -86,6 +174,7 @@ gdm_remote_display_finalize (GObject *object)
 {
         GdmRemoteDisplay *display = GDM_REMOTE_DISPLAY (object);
 
+        g_clear_pointer (&display->pending_export, pending_export_free);
         g_clear_object (&display->skeleton);
 
         G_OBJECT_CLASS (gdm_remote_display_parent_class)->finalize (object);
@@ -116,6 +205,7 @@ gdm_remote_display_class_init (GdmRemoteDisplayClass *klass)
         object_class->finalize = gdm_remote_display_finalize;
 
         display_class->prepare = gdm_remote_display_prepare;
+        display_class->export = gdm_remote_display_export;
 }
 
 static void
