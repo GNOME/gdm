GNOME/gdm

commit 85982d61790dbbf940c12ad6e747bb1ada0b1355

local-display-factory: look for 'boot_display' sysfs attr

daemon/gdm-local-display-factory.c
@@ -424,6 +424,7 @@ udev_is_settled (GdmLocalDisplayFactory *factory)
                 const gchar *id_path = g_udev_device_get_property (device, "ID_PATH");
                 g_autoptr (GUdevDevice) platform_device = NULL;
                 g_autoptr (GUdevDevice) pci_device = NULL;
+                g_autoptr (GUdevDevice) drm_device = NULL;
 
                 if (g_strrstr (id_path, "platform-simple-framebuffer") != NULL) {
                         node = next_node;
@@ -451,10 +452,28 @@ udev_is_settled (GdmLocalDisplayFactory *factory)
                                  factory->seat0_has_boot_up_graphics = TRUE;
                                  is_settled = TRUE;
                                  break;
-                        } else {
-                                 g_debug ("GdmLocalDisplayFactory: Found secondary PCI graphics adapter, not proceeding yet.");
                         }
                 }
+
+                drm_device = g_udev_device_get_parent_with_subsystem (device, "drm", NULL);
+
+                if (drm_device != NULL) {
+                        gboolean boot_display;
+
+                        boot_display = g_udev_device_get_sysfs_attr_as_int (drm_device, "boot_display");
+
+                        if (boot_display == 1) {
+                                 g_debug ("GdmLocalDisplayFactory: Found primary PCI graphics adapter, proceeding.");
+                                 factory->seat0_has_boot_up_graphics = TRUE;
+                                 is_settled = TRUE;
+                                 break;
+                        }
+                }
+
+                if (pci_device != NULL || drm_device != NULL) {
+                        g_debug ("GdmLocalDisplayFactory: Found secondary PCI graphics adapter, not proceeding yet.");
+                }
+
                 node = next_node;
         }
 

commit f4021ae365dc745f5439fab7cf6748fde78557ea

local-display-factory: Read boot_display from the DRM device
The boot_display sysfs attribute belongs to the DRM device itself.
Looking for a DRM parent misses the attribute when the enumerated device
is a card whose parent is a PCI device. Enumerating a connector can mask
the problem because its DRM parent is the card.

Read boot_display directly from the enumerated device so that a marked
card is sufficient to pass the graphics readiness check without relying
on a connector being present. Keep the PCI-parent boot_vga check.

Alessandro Astone pointed out the corresponding GDM lookup and how
connector enumeration could mask the issue. Mario Limonciello suggested
checking GDM and submitting a corresponding fix.

Fixes: 85982d61790d ("local-display-factory: look for 'boot_display' sysfs attr")
Link: https://gitlab.gnome.org/GNOME/gdm/-/merge_requests/343
Link: https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5341#note_2874583
Part-of: <https://gitlab.gnome.org/GNOME/gdm/-/merge_requests/401>

daemon/gdm-local-display-factory.c
@@ -431,7 +431,7 @@ udev_is_settled (GdmLocalDisplayFactory *factory)
                 const gchar *id_path = g_udev_device_get_property (device, "ID_PATH");
                 g_autoptr (GUdevDevice) platform_device = NULL;
                 g_autoptr (GUdevDevice) pci_device = NULL;
-                g_autoptr (GUdevDevice) drm_device = NULL;
+                gboolean boot_display;
 
                 if (g_strrstr (id_path, "platform-simple-framebuffer") != NULL) {
                         node = next_node;
@@ -462,22 +462,16 @@ udev_is_settled (GdmLocalDisplayFactory *factory)
                         }
                 }
 
-                drm_device = g_udev_device_get_parent_with_subsystem (device, "drm", NULL);
+                boot_display = g_udev_device_get_sysfs_attr_as_int (device, "boot_display");
 
-                if (drm_device != NULL) {
-                        gboolean boot_display;
-
-                        boot_display = g_udev_device_get_sysfs_attr_as_int (drm_device, "boot_display");
-
-                        if (boot_display == 1) {
-                                 g_debug ("GdmLocalDisplayFactory: Found primary PCI graphics adapter, proceeding.");
-                                 factory->seat0_has_boot_up_graphics = TRUE;
-                                 is_settled = TRUE;
-                                 break;
-                        }
+                if (boot_display == 1) {
+                        g_debug ("GdmLocalDisplayFactory: Found primary graphics adapter, proceeding.");
+                        factory->seat0_has_boot_up_graphics = TRUE;
+                        is_settled = TRUE;
+                        break;
                 }
 
-                if (pci_device != NULL || drm_device != NULL) {
+                if (pci_device != NULL) {
                         g_debug ("GdmLocalDisplayFactory: Found secondary PCI graphics adapter, not proceeding yet.");
                 }
 